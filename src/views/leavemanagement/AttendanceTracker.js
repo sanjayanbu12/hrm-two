2511a70069b79098e8db51bd217e0882b4a0cf1a
SanjayAnbu22 <117885589+SanjayAnbu22@users.noreply.github.com>
2023-06-21 18:01:59 +0530

    wfh

--- src/views/leavemanagement/AttendanceTracker.js
+++ src/views/leavemanagement/AttendanceTracker.js
@@ -1,90 +1,9 @@
-import React, { useState } from 'react';
-import {
-  Grid,
-  TextField,
-  Button,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper
-} from '@mui/material';
+import React from 'react'
 
 const AttendanceTracker = () => {
-  const [name, setName] = useState('');
-  const [date, setDate] = useState('');
-  const [attendance, setAttendance] = useState([]);
-
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-
-  const handleDateChange = (e) => {
-    setDate(e.target.value);
-  };
-
-  const handleAddAttendance = () => {
-    const newAttendance = {
-      name,
-      date
-    };
-
-    setAttendance((prevAttendance) => [...prevAttendance, newAttendance]);
-    setName('');
-    setDate('');
-  };
-
   return (
-    <div>
-      <h1>Attendance Tracker</h1>
+    <div>AttendanceTracker</div>
+  )
+}
 
-      <Grid container spacing={2} alignItems="center">
-        <Grid item xs={6}>
-          <TextField
-            label="Name"
-            variant="outlined"
-            value={name}
-            onChange={handleNameChange}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            label=""
-            variant="outlined"
-            type="date"
-            value={date}
-            onChange={handleDateChange}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant="contained" onClick={handleAddAttendance}>
-            Add Attendance
-          </Button>
-        </Grid>
-      </Grid>
-
-      <TableContainer component={Paper} style={{ marginTop: '20px' }}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Date</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {attendance.map((entry, index) => (
-              <TableRow key={index}>
-                <TableCell>{entry.name}</TableCell>
-                <TableCell>{entry.date}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-  );
-};
-
-export default AttendanceTracker;
+export default AttendanceTracker
