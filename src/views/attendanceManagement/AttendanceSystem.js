74bfd1b121ce46d3255c83191d9dbd30526eb3b0
Kannan <72759290+Kannan-S-Official@users.noreply.github.com>
2023-07-05 14:19:23 +0530

    setup added

--- src/views/attendanceManagement/AttendanceSystem.js
+++ src/views/attendanceManagement/AttendanceSystem.js
@@ -1,34 +1,64 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import MainCard from 'ui-component/cards/MainCard';
+import { Box } from '@mui/system';
+import { Typography, Button } from '@mui/material';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import HomeIcon from '@mui/icons-material/Home';
 
 const AttendanceSystem = () => {
   const [checkInTime, setCheckInTime] = useState(null);
   const [checkOutTime, setCheckOutTime] = useState(null);
 
-  const handleCheckIn = () => {
-    const currentTime = new Date().toLocaleTimeString();
-    setCheckInTime(currentTime);
+  const handleCheckIn = async () => {
+    try {
+      await axios.post('https://hrm-backend-square.onrender.com/api/attendance/checkin', { checkInTime });
+      const currentTime = new Date().toLocaleTimeString();
+      setCheckInTime(currentTime);
+      console.log(currentTime);
+      // Perform any necessary actions after successful check-in
+    } catch (error) {
+      console.error('Error performing check-in:', error);
+      // Handle error
+    }
   };
 
-  const handleCheckOut = () => {
-    const currentTime = new Date().toLocaleTimeString();
-    setCheckOutTime(currentTime);
+  const handleCheckOut = async () => {
+    try {
+      await axios.post('https://hrm-backend-square.onrender.com/api/attendance/checkout', checkOutTime);
+      const currentTime = new Date().toLocaleTimeString();
+      setCheckOutTime(currentTime);
+      console.log('Check-out successful');
+      // Perform any necessary actions after successful check-out
+    } catch (error) {
+      console.error('Error performing check-out:', error);
+      // Handle error
+    }
   };
 
   return (
     <MainCard title="Attendance Regulation">
-      <div>
-        <h2>Check-in Time: {checkInTime || 'Not Checked In'}</h2>
-        <h2>Check-out Time: {checkOutTime || 'Not Checked Out'}</h2>
-      </div>
-      <div>
+      <Box>
+        <Typography variant="h3" gutterBottom>
+          Check-in Time: {checkInTime || 'Not Checked In'}
+        </Typography>
+        <Typography variant="h3" gutterBottom>
+          Check-out Time: {checkOutTime || 'Not Checked Out'}
+        </Typography>
+      </Box>
+      <Box>
         {!checkInTime && (
-          <button onClick={handleCheckIn}>Check In</button>
+          <Button variant="contained" color="success" onClick={handleCheckIn} startIcon={<AlarmIcon />}>
+            Check In
+          </Button>
         )}
         {checkInTime && !checkOutTime && (
-          <button onClick={handleCheckOut}>Check Out</button>
+          <Button variant="contained" color="success" onClick={handleCheckOut} endIcon={<HomeIcon />}>
+            Check Out
+          </Button>
         )}
-      </div>
+        {checkInTime && checkOutTime && <p>Attendance already checked</p>}
+      </Box>
     </MainCard>
   );
 };
