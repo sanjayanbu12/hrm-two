e7fe28bbe983a25260615826139eaf32f52d5ecc
NaveenaSnsce <113565793+NaveenaSnsce@users.noreply.github.com>
2023-07-05 18:04:31 +0530

    action approve leave

--- src/views/leavemanagement/ApproveLeave.js
+++ src/views/leavemanagement/ApproveLeave.js
@@ -13,11 +13,20 @@ import {
   FormControl,
   Select,
   MenuItem,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
 } from '@mui/material';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const ApproveLeave = () => {
   const [leaveList, setLeaveList] = useState([]);
+  const [selectedLeave, setSelectedLeave] = useState(null);
+  const [openDialog, setOpenDialog] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchData();
@@ -25,26 +34,51 @@ const ApproveLeave = () => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('https://hrm-backend-square.onrender.com/api/leave'); // Update the API endpoint to the deployed backend's URL
+      const response = await axios.get('https://hrm-backend-square.onrender.com/api/leave');
       setLeaveList(response.data);
     } catch (error) {
       console.log('Error retrieving leave data:', error);
     }
   };
 
-  // ...
+  const handleActionChange = async (leaveId, action) => {
+    try {
+      if (action === 'pending') {
+        const selectedLeave = leaveList.find((leave) => leave._id === leaveId);
+        setSelectedLeave(selectedLeave);
+        setOpenDialog(true);
+      } else {
+        await updateLeaveStatus(leaveId, action);
+      }
+    } catch (error) {
+      console.log(`Error ${action === 'approve' ? 'approving' : 'rejecting'} leave:`, error);
+    }
+  };
 
-const handleActionChange = async (leaveId, action) => {
-  try {
-    const selectedAction = action; // Store the selected action in a constant variable
-    await axios.patch(`https://hrm-backend-square.onrender.com/api/leave/${leaveId}`, { status: selectedAction });
-    fetchData();
-  } catch (error) {
-    console.log(`Error ${action === 'approve' ? 'approving' : 'rejecting'} leave:`, error);
-  }
-};
+  const updateLeaveStatus = async (leaveId, status) => {
+    try {
+      await axios.patch(`https://hrm-backend-square.onrender.com/api/leave/${leaveId}`, { status });
+      fetchData();
+    } catch (error) {
+      console.log(`Error ${status === 'approve' ? 'approving' : 'rejecting'} leave:`, error);
+    }
+  };
 
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
 
+  const handleApproveLeave = () => {
+    updateLeaveStatus(selectedLeave._id, 'approve');
+    setOpenDialog(false);
+    navigate('/leavetrackerlist'); // Navigate to the LeaveTrackerList component after leave is approved
+  };
+
+  const handleRejectLeave = () => {
+    updateLeaveStatus(selectedLeave._id, 'reject');
+    setOpenDialog(false);
+    navigate('/leavetrackerlist'); // Navigate to the LeaveTrackerList component after leave is rejected
+  };
 
   const formatId = (id) => {
     return id.toString().padStart(4, '0');
@@ -64,7 +98,7 @@ const handleActionChange = async (leaveId, action) => {
                     <TableCell>Start Date</TableCell>
                     <TableCell>End Date</TableCell>
                     <TableCell>Reason</TableCell>
-                    <TableCell>Status</TableCell>
+                    <TableCell style={{ paddingLeft: '10px' }}>Status</TableCell> {/* Apply left padding */}
                   </TableRow>
                 </TableHead>
                 <TableBody>
@@ -87,8 +121,7 @@ const handleActionChange = async (leaveId, action) => {
                               <MenuItem value="" disabled>
                                 Select Action
                               </MenuItem>
-                              <MenuItem value="approve">Approve</MenuItem>
-                              <MenuItem value="reject">Reject</MenuItem>
+                              <MenuItem value="pending">Pending</MenuItem>
                             </Select>
                           </FormControl>
                         </Box>
@@ -103,6 +136,33 @@ const handleActionChange = async (leaveId, action) => {
           )}
         </Grid>
       </Grid>
+
+      {/* Leave Details Dialog */}
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogTitle>Leave Details</DialogTitle>
+        <DialogContent>
+          {selectedLeave && (
+            <div>
+              <p>Leave ID: {formatId(selectedLeave._id)}</p>
+              <p>Leave Type: {selectedLeave.leaveType}</p>
+              <p>Start Date: {selectedLeave.startDate}</p>
+              <p>End Date: {selectedLeave.endDate}</p>
+              <p>Reason: {selectedLeave.reason}</p>
+            </div>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleApproveLeave} color="primary">
+            Approve
+          </Button>
+          <Button onClick={handleRejectLeave} color="secondary">
+            Reject
+          </Button>
+          <Button onClick={handleCloseDialog} color="inherit">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
     </MainCard>
   );
 };
