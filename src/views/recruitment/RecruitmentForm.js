134d262fb642f02c07ea8c23fef74f88a299aa2c
Prakash5072 <128066591+Prakash5072@users.noreply.github.com>
2023-07-10 12:48:28 +0530

    update Form

--- src/views/recruitment/RecruitmentForm.js
+++ src/views/recruitment/RecruitmentForm.js
@@ -201,9 +201,9 @@ const RecruitmentForm = () => {
     setSkills(value.join(','));
     setErrors((prev) => ({
       ...prev,
-      skills: ''
-    }));
-  };
+      Skills: ''
+    }))
+  }
   const handleDescription = e => {
     setDescription(e.target.value)
     setErrors(prev => ({
