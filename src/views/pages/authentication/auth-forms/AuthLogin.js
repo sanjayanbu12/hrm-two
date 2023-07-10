7c47084b9c4d244c9c6da69f34fca5dfc66fb607
AjayB101 <ajayjadec@gmail.com>
2023-07-10 14:05:32 +0530

    signup-singin links added

--- src/views/pages/authentication/auth-forms/AuthLogin.js
+++ src/views/pages/authentication/auth-forms/AuthLogin.js
@@ -1,4 +1,5 @@
 import { Visibility, VisibilityOff } from '@mui/icons-material';
+// import { userSchema } from 'validation/Validation';
 import {
   Button,
   // Stack,
@@ -6,11 +7,10 @@ import {
   IconButton,
   InputAdornment,
   Grid,
-  Typography
 } from '@mui/material';
-import { useEffect, useState } from 'react';
-import { userSchema } from 'validation/Validation';
-import * as yup from 'yup';
+import { useState } from 'react';
+// /import { userSchema } from 'validation/Validation';//
+// import * as yup from 'yup';
 import axios from 'axios';
 
 // third party
@@ -22,16 +22,36 @@ import { useNavigate } from 'react-router';
 
 // ============================|| FIREBASE - LOGIN ||============================ //
 
-const FirebaseLogin = ({ ...others }) => {
+const AuthLogin = () => {
 
   const [value1, setvalue1] = useState('');
   const [value2, setvalue2] = useState('');
-  const [loginError, setLoginError] = useState('');
-  // const [botherror,setbotherror]=useState("")
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
   const [error, seterror] = useState({});
   const [showPassword, setShowPassword] = useState(false); // State variable for password visibility
   const navigate = useNavigate();
+  const validateLogin = async (e) => {
+    e.preventDefault();
+    console.log(value1, value2);
+    try {
+      
+      await axios.post('https://hrm-backend-square.onrender.com/auth/login', {
+        email: value1,
+        password: value2
+      })
+      navigate('/');
+    }
+    catch (error) {
+      if (error.response && error.response.data && error.response.data.error) {
+        seterror(error.response.data.error);
+      } else {
+        seterror('An error occurred');
+      }
+      setTimeout(() => {
+        seterror("")
+      }, 5000);
+    }
+  };
 
   const handleEmail = (e) => {
     setvalue1(e.target.value);
@@ -40,7 +60,6 @@ const FirebaseLogin = ({ ...others }) => {
       email: "",
     }));
   };
-
   const handlePass = (e) => {
     setvalue2(e.target.value);
     seterror((prev) => ({
@@ -52,69 +71,12 @@ const FirebaseLogin = ({ ...others }) => {
   const togglePasswordVisibility = () => {
     setShowPassword((prev) => !prev);
   };
-
-  const validateLogin = async () => {
-    try {
-      await userSchema.validate(
-        { email: value1, password: value2 },
-        { abortEarly: false }
-      );
-      const response = await axios.get("http://localhost:3001/users");
-      setData(response.data);
-    } catch (error) {
-      if (error instanceof yup.ValidationError) {
-        const validationerr = {};
-        error.inner.forEach((err) => {
-          validationerr[err.path] = err.message;
-        });
-        seterror(validationerr);
-        console.log(validationerr);
-      } else {
-        console.log(error);
-      }
-    }
-  };
-
-  const checkLogin = () => {
-    const user = data.find((item) => item.email === value1);
-    const user1 = data.find((item) => item.password === value2);
-  
-    if (user && user1) {
-      navigate("/dashboard/default");
-    } else if (!user && user1) {
-      setLoginError("Your email is incorrect");
-    } else if (user && !user1) {
-      setLoginError("Your password is incorrect");
-    } else {
-      setLoginError("Both email and password are incorrect");
-    }
-  
-    if (!value1) {
-      setLoginError(" ");
-    } else if (!value2) {
-      setLoginError(" ");
-    }
-  };
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:3001/users");
-        setData(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-
   return (
-    <>
-      <form noValidate {...others}>
+    <div>
+      {/* <form noValidate {...others}> */}
       <Grid >
         <TextField
-          sx={{mb:0,  height:"8 vh",}}
+          sx={{ mb: 0, height: "8 vh", }}
           id="outlined-adornment-email-login"
           label="Email Address"
           variant="outlined"
@@ -131,14 +93,14 @@ const FirebaseLogin = ({ ...others }) => {
           value={value1}
           onChange={(e) => handleEmail(e)}
         />
-        </Grid>
-<Grid>
+      </Grid>
+      <Grid>
         <TextField
           sx={{
-    
-           height:"8 vh",
+
+            height: "8 vh",
             mb: 2,
-            
+
           }}
           InputProps={{
             endAdornment: (
@@ -161,11 +123,11 @@ const FirebaseLogin = ({ ...others }) => {
           error={error && error.password}
           helperText={error && error.password}
           value={value2}
-          onChange={handlePass}
+          onChange={(e) => handlePass(e)}
         />
-        </Grid>
+      </Grid>
 
-        {/* <Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1}>
+      {/* <Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1}>
           <FormControlLabel
             control={<Checkbox checked={checked} onChange={(event) => setChecked(event.target.checked)} name="checked" color="primary" />}
             label="Remember me"
@@ -174,25 +136,23 @@ const FirebaseLogin = ({ ...others }) => {
             Forgot Password?
           </Typography>
         </Stack> */}
-       <Typography color="error" variant="body2" sx={{mb: 1,ml:10.5 }}>
-          {loginError}
-        
-        </Typography>
-<Grid >
+      {/* <Typography color="error" variant="body2" sx={{mb: 1,ml:10.5 }}>
+          {loginError} */}
+
+      {/* </Typography> */}
+      <Grid >
         <AnimateButton>
-          <Button disableElevation fullWidth size="large" type="submit" variant="contained" color="secondary"  
-             onClick={(e) => {
-              validateLogin().then(checkLogin)
-              e.preventDefault()
-            }}
-            >
+          <Button disableElevation fullWidth size="large" type="submit" variant="contained" color="secondary"
+            onClick={
+              validateLogin}
+          >
             Sign in
           </Button>
         </AnimateButton>
-        </Grid>
-      </form>
-    </>
-  );
+      </Grid>
+      {/* </form> */}
+    </div>
+  )
 };
 
-export default FirebaseLogin;
+export default AuthLogin;
