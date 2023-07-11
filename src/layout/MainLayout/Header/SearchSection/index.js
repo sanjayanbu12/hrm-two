7c4a066318641e81d620de8068fa497c9e155e7f
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-07-12 00:10:11 +0530

    checkin&checkout added

--- src/layout/MainLayout/Header/SearchSection/index.js
+++ src/layout/MainLayout/Header/SearchSection/index.js
@@ -1,192 +1,98 @@
-import PropTypes from 'prop-types';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button, Grid } from '@mui/material';
+import axios from 'axios';
 
-// material-ui
-import { useTheme, styled } from '@mui/material/styles';
-import { Avatar, Box, ButtonBase, Card, Grid, InputAdornment, OutlinedInput, Popper } from '@mui/material';
+const AttendanceTracker = () => {
+  const [checkInTime, setCheckInTime] = useState('');
+  const [checkOutTime, setCheckOutTime] = useState('');
 
-// third-party
-import PopupState, { bindPopper, bindToggle } from 'material-ui-popup-state';
+  useEffect(() => {
+    const fetchAttendance = async () => {
+      try {
+        const response = await axios.get('https://hrm-backend-square.onrender.com/attendance');
+        const { checkInTime, checkOutTime } = response.data;
 
-// project imports
-import Transitions from 'ui-component/extended/Transitions';
+        if (checkInTime) {
+          setCheckInTime(checkInTime);
+        }
 
-// assets
-import { IconAdjustmentsHorizontal, IconSearch, IconX } from '@tabler/icons';
-import { shouldForwardProp } from '@mui/system';
+        if (checkOutTime) {
+          setCheckOutTime(checkOutTime);
+        }
+      } catch (error) {
+        console.error('Error fetching attendance:', error);
+      }
+    };
 
-// styles
-const PopperStyle = styled(Popper, { shouldForwardProp })(({ theme }) => ({
-  zIndex: 1100,
-  width: '99%',
-  top: '-55px !important',
-  padding: '0 12px',
-  [theme.breakpoints.down('sm')]: {
-    padding: '0 10px'
-  }
-}));
+    fetchAttendance();
+  }, []);
 
-const OutlineInputStyle = styled(OutlinedInput, { shouldForwardProp })(({ theme }) => ({
-  width: 434,
-  marginLeft: 16,
-  paddingLeft: 16,
-  paddingRight: 16,
-  '& input': {
-    background: 'transparent !important',
-    paddingLeft: '4px !important'
-  },
-  [theme.breakpoints.down('lg')]: {
-    width: 250
-  },
-  [theme.breakpoints.down('md')]: {
-    width: '100%',
-    marginLeft: 4,
-    background: '#fff'
-  }
-}));
+  const handleCheckIn = async () => {
+    try {
+      const currentTime = new Date().toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata' });
+      const currentDate = new Date().toLocaleDateString('en-IN', { timeZone: 'Asia/Kolkata' });
+      const storedDate = localStorage.getItem('attendanceDate');
 
-const HeaderAvatarStyle = styled(Avatar, { shouldForwardProp })(({ theme }) => ({
-  ...theme.typography.commonAvatar,
-  ...theme.typography.mediumAvatar,
-  background: theme.palette.secondary.light,
-  color: theme.palette.secondary.dark,
-  '&:hover': {
-    background: theme.palette.secondary.dark,
-    color: theme.palette.secondary.light
-  }
-}));
+      if (storedDate === currentDate) {
+        // Already checked in today
+        return;
+      }
 
-// ==============================|| SEARCH INPUT - MOBILE||============================== //
+      await axios.post('https://hrm-backend-square.onrender.com/attendance/checkin', {
+        checkInTime: currentTime,
+        checkInDate: currentDate
+      });
 
-const MobileSearch = ({ value, setValue, popupState }) => {
-  const theme = useTheme();
+      setCheckInTime(currentTime);
+      localStorage.setItem('checkInTime', currentTime);
+      localStorage.setItem('attendanceDate', currentDate);
+      console.log(checkInTime);
+    } catch (error) {
+      console.error('Error performing check-in:', error);
+      // Handle error
+    }
+  };
+
+  const handleCheckOut = async () => {
+    try {
+      const currentTime = new Date().toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata' });
+      const currentDate = new Date().toLocaleDateString('en-IN', { timeZone: 'Asia/Kolkata' });
+      const storedDate = localStorage.getItem('attendanceDate');
+
+      if (storedDate !== currentDate) {
+        // Haven't checked in today
+        return;
+      }
+
+      await axios.post('https://hrm-backend-square.onrender.com/attendance/checkout', {
+        checkOutTime: currentTime,
+        checkOutDate: currentDate
+      });
+
+      setCheckOutTime(currentTime);
+      localStorage.setItem('checkOutTime', currentTime);
+      console.log(checkOutTime);
+    } catch (error) {
+      console.error('Error performing check-out:', error);
+      // Handle error
+    }
+  };
 
   return (
-    <OutlineInputStyle
-      id="input-search-header"
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-      placeholder="Search"
-      startAdornment={
-        <InputAdornment position="start">
-          <IconSearch stroke={1.5} size="1rem" color={theme.palette.grey[500]} />
-        </InputAdornment>
-      }
-      endAdornment={
-        <InputAdornment position="end">
-          <ButtonBase sx={{ borderRadius: '12px' }}>
-            <HeaderAvatarStyle variant="rounded">
-              <IconAdjustmentsHorizontal stroke={1.5} size="1.3rem" />
-            </HeaderAvatarStyle>
-          </ButtonBase>
-          <Box sx={{ ml: 2 }}>
-            <ButtonBase sx={{ borderRadius: '12px' }}>
-              <Avatar
-                variant="rounded"
-                sx={{
-                  ...theme.typography.commonAvatar,
-                  ...theme.typography.mediumAvatar,
-                  background: theme.palette.orange.light,
-                  color: theme.palette.orange.dark,
-                  '&:hover': {
-                    background: theme.palette.orange.dark,
-                    color: theme.palette.orange.light
-                  }
-                }}
-                {...bindToggle(popupState)}
-              >
-                <IconX stroke={1.5} size="1.3rem" />
-              </Avatar>
-            </ButtonBase>
-          </Box>
-        </InputAdornment>
-      }
-      aria-describedby="search-helper-text"
-      inputProps={{ 'aria-label': 'weight' }}
-    />
+    <Grid container justifyContent="center" spacing={2}>
+      <Grid item>
+        <Button variant="contained" color="success" onClick={handleCheckIn}>
+          Check In
+        </Button>
+      </Grid>
+
+      <Grid item>
+        <Button variant="contained" color="error" onClick={handleCheckOut}>
+          Check Out
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
-MobileSearch.propTypes = {
-  value: PropTypes.string,
-  setValue: PropTypes.func,
-  popupState: PopupState
-};
-
-// ==============================|| SEARCH INPUT ||============================== //
-
-const SearchSection = () => {
-  const theme = useTheme();
-  const [value, setValue] = useState('');
-
-  return (
-    <>
-      <Box sx={{ display: { xs: 'block', md: 'none' } }}>
-        <PopupState variant="popper" popupId="demo-popup-popper">
-          {(popupState) => (
-            <>
-              <Box sx={{ ml: 2 }}>
-                <ButtonBase sx={{ borderRadius: '12px' }}>
-                  <HeaderAvatarStyle variant="rounded" {...bindToggle(popupState)}>
-                    <IconSearch stroke={1.5} size="1.2rem" />
-                  </HeaderAvatarStyle>
-                </ButtonBase>
-              </Box>
-              <PopperStyle {...bindPopper(popupState)} transition>
-                {({ TransitionProps }) => (
-                  <>
-                    <Transitions type="zoom" {...TransitionProps} sx={{ transformOrigin: 'center left' }}>
-                      <Card
-                        sx={{
-                          background: '#fff',
-                          [theme.breakpoints.down('sm')]: {
-                            border: 0,
-                            boxShadow: 'none'
-                          }
-                        }}
-                      >
-                        <Box sx={{ p: 2 }}>
-                          <Grid container alignItems="center" justifyContent="space-between">
-                            <Grid item xs>
-                              <MobileSearch value={value} setValue={setValue} popupState={popupState} />
-                            </Grid>
-                          </Grid>
-                        </Box>
-                      </Card>
-                    </Transitions>
-                  </>
-                )}
-              </PopperStyle>
-            </>
-          )}
-        </PopupState>
-      </Box>
-      {/* <Box sx={{ display: { xs: 'none', md: 'block' } }}>
-        <OutlineInputStyle
-          id="input-search-header"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          placeholder="Search"
-          startAdornment={
-            <InputAdornment position="start">
-              <IconSearch stroke={1.5} size="1rem" color={theme.palette.grey[500]} />
-            </InputAdornment>
-          }
-          endAdornment={
-            <InputAdornment position="end">
-              <ButtonBase sx={{ borderRadius: '12px' }}>
-                <HeaderAvatarStyle variant="rounded">
-                  <IconAdjustmentsHorizontal stroke={1.5} size="1.3rem" />
-                </HeaderAvatarStyle>
-              </ButtonBase>
-            </InputAdornment>
-          }
-          aria-describedby="search-helper-text"
-          inputProps={{ 'aria-label': 'weight' }}
-        />
-      </Box> */}
-    </>
-  );
-};
-
-export default SearchSection;
+export default AttendanceTracker;
