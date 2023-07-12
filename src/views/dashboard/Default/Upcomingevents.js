9852117d8d71067b1cac7fad081a5eef7b5d9258
SanjayAnbu22 <134280780+sanjayanbu12@users.noreply.github.com>
2023-07-12 17:12:17 +0530

    carousel

--- src/views/dashboard/Default/Upcomingevents.js
+++ src/views/dashboard/Default/Upcomingevents.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import MainCard from 'ui-component/cards/MainCard';
 import { CardContent, Grid, Typography, Button, Box } from '@mui/material';
-import EventIcon from '@mui/icons-material/Event';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import EventAvailableIcon from '@mui/icons-material/EventAvailable';
+// import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import { useTheme } from '@mui/material/styles';
-import AddIcon from '@mui/icons-material/Add';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const Upcomingevents = () => {
   const [events, setEvents] = useState([]);
@@ -30,7 +33,7 @@ const Upcomingevents = () => {
         <MainCard content={false}>
           <CardContent>
             <Typography variant="h4" style={{ marginBottom: '20px' }}>
-              Upcoming Events
+              <b>Upcoming Events</b>
             </Typography>
             <Box sx={{ flexGrow: 1, justifyContent: 'flex-end', display: 'flex' }}>
               <Button
@@ -46,12 +49,12 @@ const Upcomingevents = () => {
                   right: '10px',
                 }}
               >
-                <AddIcon />
-                New Event
+                All Events
+                <ArrowForwardIcon />
               </Button>
             </Box>
 
-            <Grid container spacing={2} style={{ gap: '30px' }}>
+            <Carousel showArrows={true} showThumbs={false}>
               {events.map((event, index) => {
                 const startDate = new Date(event.start);
                 const endDate = new Date(event.end);
@@ -60,43 +63,44 @@ const Upcomingevents = () => {
                 const startTime = startDate.toLocaleTimeString([], { hour: 'numeric', minute: 'numeric' });
                 const endTime = endDate.toLocaleTimeString([], { hour: 'numeric', minute: 'numeric' });
                 return (
-                  <Grid
-                    item
-                    xs={12}
-                    sm={6}
-                    md={2}
-                    lg={2}
-                    key={index}
-                    style={{
-                      backgroundColor: '#f0f0f0',
-                      borderRadius: '15px',
-                      padding: '15px',
-                      display: 'flex',
-                      flexDirection: 'column',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      textAlign: 'center',
-                    }}
-                  >
-                    <EventIcon />
-                    <Typography variant="body1" style={{ fontWeight: 'bold', marginBottom: '10px' }}>
-                      {event.title}
-                    </Typography>
-                    <Typography variant="body2" style={{ marginBottom: '10px' }}>
-                      {startMonth} {startDate.getDate()} - {endMonth} {endDate.getDate()}, {endDate.getFullYear()}
-                    </Typography>
-                    <Typography variant="body2" style={{ color: '#666' }}>
-                      {startTime} - {endTime}
-                    </Typography>
-                    {event.eventLink && (
-                      <a href={event.eventLink} target="_blank" rel="noopener noreferrer">
-                        Event Link
-                      </a>
-                    )}
-                  </Grid>
+                  <div key={index}>
+                    <Grid
+                      item
+                      xs={12}
+                      sm={6}
+                      md={2}
+                      lg={2}
+                      style={{
+                        backgroundColor: '#f0f0f0',
+                        borderRadius: '15px',
+                        padding: '15px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        textAlign: 'center',
+                      }}
+                    >
+                      <EventAvailableIcon />
+                      <Typography variant="body1" style={{ fontWeight: 'bold', marginBottom: '10px' }}>
+                        {event.title}
+                      </Typography>
+                      <Typography variant="body2" style={{ marginBottom: '10px' }}>
+                        {startMonth} {startDate.getDate()} - {endMonth} {endDate.getDate()}, {endDate.getFullYear()}
+                      </Typography>
+                      <Typography variant="body2" style={{ color: '#666' }}>
+                        {startTime} - {endTime}
+                      </Typography>
+                      {event.eventLink && (
+                        <a href={event.eventLink} target="_blank" rel="noopener noreferrer">
+                          Event Link
+                        </a>
+                      )}
+                    </Grid>
+                  </div>
                 );
               })}
-            </Grid>
+            </Carousel>
           </CardContent>
         </MainCard>
       )}
