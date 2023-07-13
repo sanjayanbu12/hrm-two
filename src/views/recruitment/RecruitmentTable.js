fa5a09019b55ab3e783e4a71bad9ff91ffd46023
Prakash5072 <128066591+Prakash5072@users.noreply.github.com>
2023-07-13 10:40:28 +0530

    search

--- src/views/recruitment/RecruitmentTable.js
+++ src/views/recruitment/RecruitmentTable.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import MainCard from 'ui-component/cards/MainCard'
-import '../recruitment/Recruitment.css'
+import React, { useState, useEffect } from 'react';
+import MainCard from 'ui-component/cards/MainCard';
 import {
   Table,
   TableCell,
@@ -21,84 +20,84 @@ import {
   InputAdornment,
   Tooltip,
   Pagination
-} from '@mui/material'
-import axios from 'axios'
-import AddIcon from '@mui/icons-material/Add'
-import { useTheme } from '@mui/material/styles'
-import { useNavigate } from 'react-router'
-import { GridArrowDownwardIcon, GridArrowUpwardIcon, GridDeleteIcon, GridSearchIcon } from '@mui/x-data-grid'
-import Swal from 'sweetalert2'
-import { Edit } from '@mui/icons-material'
-import VisibilityIcon from '@mui/icons-material/Visibility'
+} from '@mui/material';
+import axios from 'axios';
+import AddIcon from '@mui/icons-material/Add';
+import { useTheme } from '@mui/material/styles';
+import { useNavigate } from 'react-router';
+import { GridArrowDownwardIcon, GridArrowUpwardIcon, GridDeleteIcon, GridSearchIcon } from '@mui/x-data-grid';
+import Swal from 'sweetalert2';
+import { Edit } from '@mui/icons-material';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 
 const RecruitmentTable = () => {
-  const [recruitmentList, setRecruitmentList] = useState([])
-  const [loader, setLoader] = useState(true)
-  const [open, setOpen] = useState(false)
-  const [selectedJob, setSelectedJob] = useState(null)
-  const [search, setSearch] = useState('')
-  const [sortDirection, setSortDirection] = useState('asc')
-  const theme = useTheme()
-  const navigate = useNavigate()
-  const [currentPage, setCurrentPage] = useState(1)
-  const rowsPerPage = 5
+  const [recruitmentList, setRecruitmentList] = useState([]);
+  const [loader, setLoader] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [selectedJob, setSelectedJob] = useState(null);
+  const [search, setSearch] = useState('');
+  const [sortDirection, setSortDirection] = useState('asc');
+  const theme = useTheme();
+  const navigate = useNavigate();
+  const [currentPage, setCurrentPage] = useState(1);
+  const rowsPerPage = 5;
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('https://hrm-backend-square.onrender.com/rec/getRec')
-      const newData = response.data.getData
-      setRecruitmentList(newData)
-      setLoader(false)
-      console.log(newData + ' this is the new data')
+      const response = await axios.get('https://hrm-backend-square.onrender.com/rec/getRec');
+      const newData = response.data.getData;
+      setRecruitmentList(newData);
+      setLoader(false);
+      console.log(newData + ' this is the new data');
     } catch (error) {
-      console.log('Error retrieving user data:', error)
+      console.log('Error retrieving user data:', error);
     }
-  }
+  };
 
-  const handleView = id => {
-    const job = recruitmentList.find(item => item._id === id)
-    setSelectedJob(job)
-    setOpen(true)
-  }
+  const handleView = (id) => {
+    const job = recruitmentList.find((item) => item._id === id);
+    setSelectedJob(job);
+    setOpen(true);
+  };
 
   const handleClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
 
-  const handleSearch = e => {
-    setSearch(e.target.value)
-  }
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
   const handleSort = () => {
-    const sortedList = [...recruitmentList]
+    const sortedList = [...recruitmentList];
     sortedList.sort((a, b) => {
-      const jobIDA = parseInt(a.uuid)
-      const jobIDB = parseInt(b.uuid)
+      const jobIDA = parseInt(a.uuid);
+      const jobIDB = parseInt(b.uuid);
       if (jobIDA < jobIDB) {
-        return -1
+        return -1;
       }
       if (jobIDA > jobIDB) {
-        return 1
+        return 1;
       }
-      return 0
-    })
-       sortDirection === 'asc'
-      sortedList.reverse()
-    
-    setRecruitmentList(sortedList)
-    setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc')
-  }
-  
-  const handleEdit = id => {
-    navigate(`/jobform/${id}`)
-  }
+      return 0;
+    });
+    sortDirection === 'asc';
+    sortedList.reverse();
 
-  const handleDelete = id => {
-    handleClose()
+    setRecruitmentList(sortedList);
+    setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+  };
+
+  const handleEdit = (id) => {
+    navigate(`/jobform/${id}`);
+  };
+
+  const handleDelete = (id) => {
+    handleClose();
     Swal.fire({
       icon: 'warning',
       text: 'Are you sure you want to delete this recruitment?',
@@ -107,82 +106,86 @@ const RecruitmentTable = () => {
       cancelButtonColor: '#d33',
       confirmButtonText: 'OK',
       cancelButtonText: 'Cancel'
-    }).then(async result => {
+    }).then(async (result) => {
       if (result.isConfirmed) {
         try {
-          await axios.delete(`https://hrm-backend-square.onrender.com/rec/getRec/${id}`)
-          await fetchData()
-          handleClose()
+          await axios.delete(`https://hrm-backend-square.onrender.com/rec/getRec/${id}`);
+          await fetchData();
+          handleClose();
           Swal.fire({
             icon: 'success',
             text: 'Recruitment deleted successfully.'
-          })
+          });
         } catch (error) {
-          console.log('Error deleting recruitment:', error)
+          console.log('Error deleting recruitment:', error);
         }
       }
-    })
-  }
+    });
+  };
 
-  const filteredJobs = recruitmentList.filter(job => {
-    const lowerSearchText = search.toLowerCase()
-    return Object.values(job).some(value => value && value.toString().toLowerCase().includes(lowerSearchText))
-  })
+  const filteredJobs = recruitmentList.filter((job) => {
+    const lowerSearchText = search.toLowerCase();
+    return Object.values(job).some((value) => value && value.toString().toLowerCase().includes(lowerSearchText));
+  });
   const handlePageChange = (e, value) => {
-    setCurrentPage(value)
-  }
+    setCurrentPage(value);
+  };
 
-  const indexOfLastJob = currentPage * rowsPerPage
-  const indexOfFirstJob = indexOfLastJob - rowsPerPage
-  const currentJobs = filteredJobs.slice(indexOfFirstJob, indexOfLastJob)
+  const indexOfLastJob = currentPage * rowsPerPage;
+  const indexOfFirstJob = indexOfLastJob - rowsPerPage;
+  const currentJobs = filteredJobs.slice(indexOfFirstJob, indexOfLastJob);
 
   return (
-    <MainCard title='Job Description Table'>
+    <MainCard title="Job Description Table">
       {loader ? (
-        <Box display='flex' justifyContent='center' alignItems='center' height='100vh'>
+        <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
           <CircularProgress />
         </Box>
       ) : (
         <div>
           <Box>
             <Grid container spacing={2}>
-              <Grid xs={9} sx={{marginLeft:'30px'}}>
-            <TextField className='Searchbutton'
-            label='Search'
-              variant='outlined'
-              color='info'
-              value={search}
-              onChange={handleSearch}
-              size='small'
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position='Start'>
-                    <GridSearchIcon color='primary' />
-                  </InputAdornment>
-                )
-              }}
-            /></Grid>
-           <Grid xs={2} sx={{marginLeft:'23px'}}>
-            <Button
-              onClick={() => {
-                navigate('/jobform')
-              }}
-              sx={{
-                width: '200px',
-                height: '40px',
-                borderRadius: '10px',
-                padding: 0.6,
-                background: '#673ab7',
-                color: '#efebe9',
-                '&:hover': {
-                  color: theme.palette.secondary.light,
-                  background: '#673ab7'
-                }
-              }}
-            >
-              <AddIcon />
-              Add New
-            </Button></Grid></Grid>
+              <Grid xs={9} sx={{ marginLeft: '30px' }}>
+                <TextField
+                  sx={{ width: '57px', height: '0px', transition: 'width 2s ease-in-out', '&:hover': { width: '300px'} }}
+                  label="Search" 
+                  variant="outlined"
+                  color="info"
+                  value={search}
+                  onChange={handleSearch}
+                  size="small"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        <GridSearchIcon color="primary" />
+                      </InputAdornment>
+                    )
+                  }}
+                />
+              </Grid>
+              <Grid xs={2} sx={{ marginLeft: '23px' }}>
+                <Button
+                  onClick={() => {
+                    navigate('/jobform');
+                  }}
+                  sx={{
+                    width: '200px',
+                    height: '40px',
+                    borderRadius: '10px',
+                    padding: 0.6,
+                    background: '#673ab7',
+                    color: '#efebe9',
+                    '&:hover': {
+                      color: theme.palette.secondary.light,
+                      background: '#673ab7'
+                    }
+                  }}
+                >
+                  <AddIcon />
+                  Add New
+                </Button>
+              </Grid>
+            </Grid>
           </Box>
           <Grid container spacing={2}>
             <Grid item xs={12}>
@@ -192,12 +195,12 @@ const RecruitmentTable = () => {
                     <TableHead>
                       <TableRow>
                         <TableCell width={120}>
-                          <Button color='inherit' onClick={handleSort}>
+                          <Button color="inherit" onClick={handleSort}>
                             Job ID{' '}
                             {sortDirection === 'asc' ? (
-                             <GridArrowUpwardIcon fontSize='small'/>
+                              <GridArrowUpwardIcon fontSize="small" />
                             ) : (
-                              <GridArrowDownwardIcon fontSize='small'/>
+                              <GridArrowDownwardIcon fontSize="small" />
                             )}
                           </Button>
                         </TableCell>
@@ -206,12 +209,12 @@ const RecruitmentTable = () => {
                         <TableCell>Worktype</TableCell>
                         <TableCell>Location</TableCell>
                         <TableCell>Deadline</TableCell>
-                        <TableCell align='center'>Action</TableCell>
+                        <TableCell align="center">Action</TableCell>
                       </TableRow>
                     </TableHead>
                     <TableBody>
                       {currentJobs.length > 0 ? (
-                        currentJobs.map(x => (
+                        currentJobs.map((x) => (
                           <TableRow key={x._id}>
                             <TableCell>{x.uuid}</TableCell>
                             <TableCell>{x.Jobrole}</TableCell>
@@ -219,21 +222,21 @@ const RecruitmentTable = () => {
                             <TableCell>{x.Worktype}</TableCell>
                             <TableCell>{x.Location}</TableCell>
                             <TableCell>{new Date(x.Deadline).toLocaleDateString('en-GB')}</TableCell>
-                            <TableCell align='left' sx={{ '&:hover': { cursor: 'pointer' } }}>
+                            <TableCell align="left" sx={{ '&:hover': { cursor: 'pointer' } }}>
                               <Box sx={{ display: 'flex', justifyContent: 'center', gap: '15px' }}>
-                                <Tooltip title='Click to View'>
+                                <Tooltip title="Click to View">
                                   <VisibilityIcon
-                                    fontSize='small'
+                                    fontSize="small"
                                     onClick={() => {
-                                      handleView(x._id)
+                                      handleView(x._id);
                                     }}
                                   />
                                 </Tooltip>
-                                <Tooltip title='Edit'>
-                                  <Edit fontSize='small' color='primary' onClick={() => handleEdit(x._id)} />
+                                <Tooltip title="Edit">
+                                  <Edit fontSize="small" color="primary" onClick={() => handleEdit(x._id)} />
                                 </Tooltip>
-                                <Tooltip title='Delete'>
-                                  <GridDeleteIcon fontSize='small' onClick={() => handleDelete(x._id)} color='error' />
+                                <Tooltip title="Delete">
+                                  <GridDeleteIcon fontSize="small" onClick={() => handleDelete(x._id)} color="error" />
                                 </Tooltip>
                               </Box>
                             </TableCell>
@@ -241,7 +244,7 @@ const RecruitmentTable = () => {
                         ))
                       ) : (
                         <TableRow>
-                          <TableCell colSpan={8} align='center'>
+                          <TableCell colSpan={8} align="center">
                             No data found
                           </TableCell>
                         </TableRow>
@@ -262,74 +265,72 @@ const RecruitmentTable = () => {
           </Grid>
         </div>
       )}
-
-      {/* Dialog Box */}
-      <Dialog open={open} onClose={handleClose} maxWidth='md' fullWidth>
+      <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
         {selectedJob && (
           <>
             <Box sx={{ display: 'flex', justifyContent: 'center', backgroundColor: '#ab47bc', marginBottom: '1px' }}>
-              <DialogTitle variant='h2' align='center'>
+              <DialogTitle variant="h2" align="center">
                 Job Description Details
               </DialogTitle>
             </Box>
             <Box sx={{ backgroundColor: '#f5f5f5' }}>
               <DialogContent>
                 <Box>
-                  <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
                     <b> Job Role</b>
                     <b style={{ marginLeft: '223px', paddingRight: '10px' }}>:</b>
                     {selectedJob.Jobrole}
                   </Typography>
-                  <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
                     <b>No. of Openings</b>
                     <b style={{ marginLeft: '178px', paddingRight: '10px' }}>:</b> {selectedJob.Openings}
                   </Typography>
-                  <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
                     <b> Company</b>
                     <b style={{ marginLeft: '220px', paddingRight: '10px' }}>:</b> {selectedJob.Company}
                   </Typography>
-                  <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
                     <b> Location</b>
                     <b style={{ marginLeft: '225px', paddingRight: '10px' }}>:</b> {selectedJob.Location}
                   </Typography>
-                  <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
                     <b> Worktype</b>
                     <b style={{ marginLeft: '221px', paddingRight: '10px' }}>:</b> {selectedJob.Worktype}
                   </Typography>
-                  <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
                     <b> Qualification</b>
                     <b style={{ marginLeft: '200px', paddingRight: '10px' }}>:</b> {selectedJob.Education}
                   </Typography>
-                  <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
                     <b> Year of Passing</b>
                     <b style={{ marginLeft: '180px', paddingRight: '10px' }}>:</b>{' '}
                     {!selectedJob.Year ? <span>Not Mentioned </span> : selectedJob.Year}
                   </Typography>
-                  <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
                     <b> Experience</b>
                     <b style={{ marginLeft: '211px', paddingRight: '10px' }}>:</b> {selectedJob.Experience} Years
                   </Typography>
-                  <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
                     <b> Requirements</b>
                     <b style={{ marginLeft: '193px', paddingRight: '10px' }}>:</b> {selectedJob.Requirements}
                   </Typography>
-                  <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
                     <b> Description</b>
                     <b style={{ marginLeft: '210px', paddingRight: '10px' }}>:</b> {selectedJob.Description}
                   </Typography>
-                  <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
                     <b> Last Date to Apply</b>
                     <b style={{ marginLeft: '168px', paddingRight: '10px' }}>:</b> {selectedJob.Deadline}
                   </Typography>
-                  <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
                     <b> Application Count</b>
                     <b style={{ marginLeft: '170px', paddingRight: '10px' }}>:</b>
                   </Typography>
-                  <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
                     <b> Selected</b>
                     <b style={{ marginLeft: '228px', paddingRight: '10px' }}>:</b>
                   </Typography>
-                  <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
                     <b>Remaining</b>
                     <b style={{ marginLeft: '215px', paddingRight: '10px' }}>:</b>
                   </Typography>
@@ -340,7 +341,7 @@ const RecruitmentTable = () => {
         )}
       </Dialog>
     </MainCard>
-  )
-}
+  );
+};
 
-export default RecruitmentTable
+export default RecruitmentTable;
