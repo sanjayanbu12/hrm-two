5e520ff1f14d4ec5933f8ed54c46b653bf8895c0
Prakash5072 <128066591+Prakash5072@users.noreply.github.com>
2023-07-17 11:29:25 +0530

    Tooltip

--- src/views/recruitment/ApplicationTracker.js
+++ src/views/recruitment/ApplicationTracker.js
@@ -1,59 +1,60 @@
-import { CircularProgress, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import MainCard from 'ui-component/cards/MainCard';
-import { saveAs } from 'file-saver';
-import TextSnippetIcon from '@mui/icons-material/TextSnippet';
-import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
-import { Box } from '@mui/system';
+import { CircularProgress, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip } from '@mui/material'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import MainCard from 'ui-component/cards/MainCard'
+import { saveAs } from 'file-saver'
+import TextSnippetIcon from '@mui/icons-material/TextSnippet'
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto'
+import { Box } from '@mui/system'
 const ApplicationTracker = () => {
-  const [Data, setData] = useState([]);
-  const [loader, setLoader] = useState(true);
+  const [Data, setData] = useState([])
+  const [loader, setLoader] = useState(true)
   useEffect(() => {
-    fetchData();
-  }, []);
-const downloadStyles={
-  cursor: 'pointer',
-  transform: 'scale(1.1)',
-  transition: 'transform 0.3s ease',
-  // Additional styles for hover
-  '&:hover': {
-    backgroundColor: 'blue', // Change the background color on hover
-    color: 'white', // Change the text color on hover
-    boxShadow: '0 0 5px rgba(0, 0, 0, 0.3)', // Add a shadow on hover
-  },
-}
+    fetchData()
+  }, [])
+  const downloadStyles = {
+    cursor: 'pointer',
+    transform: 'scale(1.1)',
+    transition: 'transform 0.3s ease',
+    // Additional styles for hover
+    '&:hover': {
+      backgroundColor: 'blue', // Change the background color on hover
+      color: 'white', // Change the text color on hover
+      boxShadow: '0 0 5px rgba(0, 0, 0, 0.3)' // Add a shadow on hover
+    }
+  }
   const fetchData = async () => {
     try {
-      const response = await axios.get('https://hrm-backend-square.onrender.com/ats/');
+      const response = await axios.get('https://hrm-backend-square.onrender.com/ats/')
       const newData = response.data.getData
-      setData(newData);
+      setData(newData)
       setLoader(false)
-      console.log(response.data.getData, ' this is the new data');
+      console.log(response.data.getData, ' this is the new data')
     } catch (error) {
-      console.log('Error retrieving user data:', error);
+      console.log('Error retrieving user data:', error)
     }
-  };
-  const handleResume = async (id) => {
+  }
+  const handleResume = async id => {
     try {
       const response = await axios.get(`https://hrm-backend-square.onrender.com/ats/resume/${id}`, {
-        responseType: 'arraybuffer' });
-      const byteArray = new Uint8Array(response.data);
-      const blob = new Blob([byteArray], { type: 'application/pdf' });
+        responseType: 'arraybuffer'
+      })
+      const byteArray = new Uint8Array(response.data)
+      const blob = new Blob([byteArray], { type: 'application/pdf' })
 
-      saveAs(blob,'resume.pdf');
+      saveAs(blob, 'resume.pdf')
     } catch (error) {
-      console.log('Error downloading resume:', error);
+      console.log('Error downloading resume:', error)
     }
-  };
-  const handlePhotoDown = async (id) => {
+  }
+  const handlePhotoDown = async id => {
     console.log(`id  = ${id}`)
     try {
       const response = await axios.get(`https://hrm-backend-square.onrender.com/ats/photo/${id}`, {
         responseType: 'arraybuffer'
       })
       const contentType = response.headers['Content-Type']
-      const extension = contentType === 'image/jpeg' ? 'jpeg' : 'png';
+      const extension = contentType === 'image/jpeg' ? 'jpeg' : 'png'
       console.log(`type  = ${extension}`)
       const byteArray = new Uint8Array(response.data)
       console.log(`byteArray  = ${byteArray}`)
@@ -68,52 +69,52 @@ const downloadStyles={
   return (
     <MainCard title='Application Tracker'>
       <TableContainer component={Paper}>
-        {loader ? (<Box display="flex" justifyContent="center" alignItems="center" height="100vh">
-          <CircularProgress />
-        </Box>)
-      :  
- 
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Jobrole</TableCell>
-              <TableCell>Photo</TableCell>
-              <TableCell>Resume</TableCell>
-              <TableCell>Mobile No</TableCell>
-              <TableCell>Email</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {Data.map((x) => (
-              <TableRow key={x._id}>
-                <TableCell>{x.name}</TableCell>
-                <TableCell>{x.position}</TableCell>
-                <TableCell  >
-                  {x.photo && (
-                    // <img
-                    //   src={x.photo}
-                    //   alt=" "
-                    //   style={{ width: '100px' }}
-                    // />
-                  <InsertPhotoIcon style={downloadStyles} onClick={() => handlePhotoDown(x._id)}/>
-                  )}
-                </TableCell>
-                <TableCell>
-                  {x.resume && (
-                    <TextSnippetIcon style={downloadStyles} onClick={() => handleResume(x._id)}  />
-                  )}
-                </TableCell>
-                <TableCell>{x.phone}</TableCell>
-                <TableCell>{x.email}</TableCell>
+        {loader ? (
+          <Box display='flex' justifyContent='center' alignItems='center' height='100vh'>
+            <CircularProgress />
+          </Box>
+        ) : (
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Jobrole</TableCell>
+                <TableCell>Photo</TableCell>
+                <TableCell>Resume</TableCell>
+                <TableCell>Mobile No</TableCell>
+                <TableCell>Email</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-}
+            </TableHead>
+            <TableBody>
+              {Data.map(x => (
+                <TableRow key={x._id}>
+                  <TableCell>{x.name}</TableCell>
+                  <TableCell>{x.position}</TableCell>
+                  <TableCell>
+                    {x.photo && (
+                      <Tooltip title='Download Photo'>
+                        <InsertPhotoIcon style={downloadStyles} onClick={() => handlePhotoDown(x._id)} />
+                      </Tooltip>
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {x.resume && (
+                      <Tooltip title='Download Resume'>
+                        <TextSnippetIcon style={downloadStyles} onClick={() => handleResume(x._id)} />
+                      </Tooltip>
+                    )}
+                  </TableCell>
+
+                  <TableCell>{x.phone}</TableCell>
+                  <TableCell>{x.email}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
       </TableContainer>
     </MainCard>
-  );
-};
+  )
+}
 
-export default ApplicationTracker;
+export default ApplicationTracker
