d6b6aa4b7e443b7261f8e988a267eb65a7b60f3c
Prakash5072 <128066591+Prakash5072@users.noreply.github.com>
2023-08-09 11:02:16 +0530

    Update ApplicationTracker.js

--- src/views/recruitment/ApplicationTracker.js
+++ src/views/recruitment/ApplicationTracker.js
@@ -5,14 +5,14 @@ import MainCard from 'ui-component/cards/MainCard'
 import { saveAs } from 'file-saver'
 import TextSnippetIcon from '@mui/icons-material/TextSnippet'
 import InsertPhotoIcon from '@mui/icons-material/InsertPhoto'
-import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityIcon from '@mui/icons-material/Visibility'
 
 import { Box } from '@mui/system'
 import ApplicationView from './ApplicationView'
 const ApplicationTracker = () => {
   const [Data, setData] = useState([])
-  const[selectedJob,setSelectedJob]=useState(null);
-  const [open, setOpen] = useState(false);
+  const [selectedJob, setSelectedJob] = useState(null)
+  const [open, setOpen] = useState(false)
   const [loader, setLoader] = useState(true)
   useEffect(() => {
     fetchData()
@@ -22,64 +22,63 @@ const ApplicationTracker = () => {
     transform: 'scale(1.1)',
     transition: 'transform 0.3s ease',
     '&:hover': {
-      backgroundColor: 'blue', 
+      backgroundColor: 'blue',
       color: 'white',
-      boxShadow: '0 0 5px rgba(0, 0, 0, 0.3)' 
+      boxShadow: '0 0 5px rgba(0, 0, 0, 0.3)'
     }
   }
   const fetchData = async () => {
     try {
       const response = await axios.get('https://hrm-backend-square.onrender.com/ats/')
       const newData = response.data.getData
+      console.log(newData)
       setData(newData)
       setLoader(false)
       console.log(response.data.getData, ' this is the new data')
     } catch (error) {
-      console.log('Error retrieving user data:', error)
+      console.log(newData)
     }
   }
-  const handleView = (id) => {
-    const job = Data.find((item) => item._id === id);
-    setSelectedJob(job);
-    setOpen(true);
-  };
+  const handleView = id => {
+    const job = Data.find(item => item._id === id)
+    setSelectedJob(job)
+    setOpen(true)
+  }
 
   const handleClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
   const handleResume = async (id, name) => {
     try {
       const response = await axios.get(`https://hrm-backend-square.onrender.com/ats/resume/${id}`, {
-        responseType: 'arraybuffer',
-      });
-      const byteArray = new Uint8Array(response.data);
-      const blob = new Blob([byteArray], { type: 'application/pdf' });
-      saveAs(blob, `${name} resume.pdf`);
+        responseType: 'arraybuffer'
+      })
+      const byteArray = new Uint8Array(response.data)
+      const blob = new Blob([byteArray], { type: 'application/pdf' })
+      saveAs(blob, `${name} resume.pdf`)
     } catch (error) {
-      console.log('Error downloading resume:', error);
+      console.log('Error downloading resume:', error)
     }
-  };
-  
+  }
+
   const handlePhotoDown = async (id, name) => {
-    console.log(`id  = ${id}`);
+    console.log(`id  = ${id}`)
     try {
       const response = await axios.get(`https://hrm-backend-square.onrender.com/ats/photo/${id}`, {
-        responseType: 'arraybuffer',
-      });
-      const contentType = response.headers['Content-Type'];
-      const extension = contentType === 'image/jpeg' ? 'jpeg' : 'png';
-      console.log(`type  = ${extension}`);
-      const byteArray = new Uint8Array(response.data);
-      console.log(`byteArray  = ${byteArray}`);
-      const blob = new Blob([byteArray], { type: contentType });
-      console.log(`blob  = ${blob}`);
-      saveAs(blob, `${name}.${extension}`);
+        responseType: 'arraybuffer'
+      })
+      const contentType = response.headers['Content-Type']
+      const extension = contentType === 'image/jpeg' ? 'jpeg' : 'png'
+      console.log(`type  = ${extension}`)
+      const byteArray = new Uint8Array(response.data)
+      console.log(`byteArray  = ${byteArray}`)
+      const blob = new Blob([byteArray], { type: contentType })
+      console.log(`blob  = ${blob}`)
+      saveAs(blob, `${name}.${extension}`)
     } catch (error) {
-      console.log('Error downloading resume:', error);
+      console.log('Error downloading resume:', error)
     }
-  };
-  
-  
+  }
 
   return (
     <MainCard title='Application Tracker'>
@@ -109,14 +108,14 @@ const ApplicationTracker = () => {
                   <TableCell>
                     {x.photo && (
                       <Tooltip title='Download Photo'>
-                        <InsertPhotoIcon style={downloadStyles} onClick={() => handlePhotoDown(x._id,x.name)} />
+                        <InsertPhotoIcon style={downloadStyles} onClick={() => handlePhotoDown(x._id, x.name)} />
                       </Tooltip>
                     )}
                   </TableCell>
                   <TableCell>
                     {x.resume && (
                       <Tooltip title='Download Resume'>
-                        <TextSnippetIcon style={downloadStyles} onClick={() => handleResume(x._id,x.name)} />
+                        <TextSnippetIcon style={downloadStyles} onClick={() => handleResume(x._id, x.name)} />
                       </Tooltip>
                     )}
                   </TableCell>
@@ -124,15 +123,16 @@ const ApplicationTracker = () => {
                   <TableCell>{x.phone}</TableCell>
                   <TableCell>{x.email}</TableCell>
                   <TableCell align='center'>
-                  <Tooltip title="Click to View">
-                                  <VisibilityIcon
-                                    fontSize="small"
-                                    onClick={() => {
-                                      handleView(x._id);
-                                    }}
-                                  />
-                                </Tooltip></TableCell>
-                                <ApplicationView open={open} handleClose={handleClose} selectedJob={selectedJob} />
+                    <Tooltip title='Click to View'>
+                      <VisibilityIcon
+                        fontSize='small'
+                        onClick={() => {
+                          handleView(x._id)
+                        }}
+                      />
+                    </Tooltip>
+                  </TableCell>
+                  <ApplicationView open={open} handleClose={handleClose} selectedJob={selectedJob} />
                 </TableRow>
               ))}
             </TableBody>
