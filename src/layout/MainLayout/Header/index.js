b33aee87772ab42c04a6d87bff23e80474951093
NaveenaSnsce <113565793+NaveenaSnsce@users.noreply.github.com>
2023-08-09 10:52:31 +0530

    final commit

--- src/layout/MainLayout/Header/index.js
+++ src/layout/MainLayout/Header/index.js
@@ -1,89 +1,19 @@
-// import PropTypes from 'prop-types';
-
-// // material-ui
-// import { useTheme } from '@mui/material/styles';
-// import { Avatar, Box, ButtonBase } from '@mui/material';
-
-// // project imports
-// import LogoSection from '../LogoSection';
-// import SearchSection from './SearchSection';
-// import ProfileSection from './ProfileSection';
-// import NotificationSection from './NotificationSection';
-
-// // assets
-// import { IconMenu2 } from '@tabler/icons';
-
-// // ==============================|| MAIN NAVBAR / HEADER ||============================== //
-
-// const Header = ({ handleLeftDrawerToggle }) => {
-//   const theme = useTheme();
-
-//   return (
-//     <>
-//       {/* logo & toggler button */}
-//       <Box
-//         sx={{
-//           width: 228,
-//           display: 'flex',
-//           [theme.breakpoints.down('md')]: {
-//             width: 'auto'
-//           }
-//         }}
-//       >
-//         <Box component="span" sx={{ display: { xs: 'none', md: 'block' }, flexGrow: 1 }}>
-//           <LogoSection />
-//         </Box>
-//         <ButtonBase sx={{ borderRadius: '12px', overflow: 'hidden' }}>
-//           <Avatar
-//             variant="rounded"
-//             sx={{
-//               ...theme.typography.commonAvatar,
-//               ...theme.typography.mediumAvatar,
-//               transition: 'all .2s ease-in-out',
-//               background: theme.palette.secondary.light,
-//               color: theme.palette.secondary.dark,
-//               '&:hover': {
-//                 background: theme.palette.secondary.dark,
-//                 color: theme.palette.secondary.light
-//               }
-//             }}
-//             onClick={handleLeftDrawerToggle}
-//             color="inherit"
-//           >
-//             <IconMenu2 stroke={1.5} size="1.3rem" />
-//           </Avatar>
-//         </ButtonBase>
-//       </Box>
-
-//       {/* header search */}
-//       <SearchSection />
-//       <Box sx={{ flexGrow: 1 }} />
-//       <Box sx={{ flexGrow: 1 }} />
-
-//       {/* notification & profile */}
-//       <NotificationSection />
-      
-//       <ProfileSection />
-//     </>
-//   );
-// };
-
-// Header.propTypes = {
-//   handleLeftDrawerToggle: PropTypes.func
-// };
-
-// export default Header;
-
 import PropTypes from 'prop-types';
+
+// material-ui
 import { useTheme } from '@mui/material/styles';
 import { Avatar, Box, ButtonBase } from '@mui/material';
+
+// project imports
 import LogoSection from '../LogoSection';
 import SearchSection from './SearchSection';
 import ProfileSection from './ProfileSection';
 import NotificationSection from './NotificationSection';
-import DarkLightMode from './DarkLightMode'; // Import the DarkLightMode component
 
+// assets
 import { IconMenu2 } from '@tabler/icons';
+
+// ==============================|| MAIN NAVBAR / HEADER ||============================== //
 
 const Header = ({ handleLeftDrawerToggle }) => {
   const theme = useTheme();
@@ -130,13 +60,9 @@ const Header = ({ handleLeftDrawerToggle }) => {
       <Box sx={{ flexGrow: 1 }} />
       <Box sx={{ flexGrow: 1 }} />
 
-      {/* notification */}
+      {/* notification & profile */}
       <NotificationSection />
-
-      {/* Add the DarkLightMode component here */}
-      <DarkLightMode />
-
-      {/* profile */}
+      
       <ProfileSection />
     </>
   );
@@ -147,3 +73,77 @@ Header.propTypes = {
 };
 
 export default Header;
+
+// import PropTypes from 'prop-types';
+// import { useTheme } from '@mui/material/styles';
+// import { Avatar, Box, ButtonBase } from '@mui/material';
+// import LogoSection from '../LogoSection';
+// import SearchSection from './SearchSection';
+// import ProfileSection from './ProfileSection';
+// import NotificationSection from './NotificationSection';
+// import DarkLightMode from './DarkLightMode'; // Import the DarkLightMode component
+
+// import { IconMenu2 } from '@tabler/icons';
+
+// const Header = ({ handleLeftDrawerToggle }) => {
+//   const theme = useTheme();
+
+//   return (
+//     <>
+//       {/* logo & toggler button */}
+//       <Box
+//         sx={{
+//           width: 228,
+//           display: 'flex',
+//           [theme.breakpoints.down('md')]: {
+//             width: 'auto'
+//           }
+//         }}
+//       >
+//         <Box component="span" sx={{ display: { xs: 'none', md: 'block' }, flexGrow: 1 }}>
+//           <LogoSection />
+//         </Box>
+//         <ButtonBase sx={{ borderRadius: '12px', overflow: 'hidden' }}>
+//           <Avatar
+//             variant="rounded"
+//             sx={{
+//               ...theme.typography.commonAvatar,
+//               ...theme.typography.mediumAvatar,
+//               transition: 'all .2s ease-in-out',
+//               background: theme.palette.secondary.light,
+//               color: theme.palette.secondary.dark,
+//               '&:hover': {
+//                 background: theme.palette.secondary.dark,
+//                 color: theme.palette.secondary.light
+//               }
+//             }}
+//             onClick={handleLeftDrawerToggle}
+//             color="inherit"
+//           >
+//             <IconMenu2 stroke={1.5} size="1.3rem" />
+//           </Avatar>
+//         </ButtonBase>
+//       </Box>
+
+//       {/* header search */}
+//       <SearchSection />
+//       <Box sx={{ flexGrow: 1 }} />
+//       <Box sx={{ flexGrow: 1 }} />
+
+//       {/* notification */}
+//       <NotificationSection />
+
+//       {/* Add the DarkLightMode component here */}
+//       <DarkLightMode />
+
+//       {/* profile */}
+//       <ProfileSection />
+//     </>
+//   );
+// };
+
+// Header.propTypes = {
+//   handleLeftDrawerToggle: PropTypes.func
+// };
+
+// export default Header;
