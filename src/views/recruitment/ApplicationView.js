8216e68ebe83522279b1cbfba87a77214a55217e
Prakash5072 <128066591+Prakash5072@users.noreply.github.com>
2023-08-09 15:37:13 +0530

    Update ApplicationView.js

--- src/views/recruitment/ApplicationView.js
+++ src/views/recruitment/ApplicationView.js
@@ -1,97 +1,76 @@
-import { Dialog, DialogContent, DialogTitle } from "@mui/material";
-import { Box } from "@mui/system";
-import { Typography } from "@mui/material";
-import React from "react"; 
+import { Dialog, DialogContent, DialogTitle } from '@mui/material'
+import { Box } from '@mui/system'
+import { Typography } from '@mui/material'
+import React from 'react'
 
+const ApplicationView = ({ open, handleClose, selectedJob }) => {
+  const formatDate =(date) =>{
+    const d = new Date(date)
+    const day = d.getDate().toString().padStart(2, '0')
+    const month = (d.getMonth() + 1).toString().padStart(2, '0')
+    const year = d.getFullYear()
+    return `${day}-${month}-${year}`
+  }
 
-
-const ApplicationView=({ open, handleClose, selectedJob })=>{
-
-return(
-<Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
-{selectedJob && (
-  <>
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        background: '#2196f3',
-        marginBottom: '1px'
-      }}
-    >
-      <DialogTitle variant="h2" align="center">
-        Candidate Application View
-      </DialogTitle>
-    </Box>
-    <Box sx={{ backgroundColor: '#f5f5f5' }}>
-      <DialogContent>
-        <Box>
-          <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
-            <b> Job Role</b>
-            <b style={{ marginLeft: '223px', paddingRight: '10px' }}>:</b>
-            {selectedJob.Jobrole}
-          </Typography>
-          <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
-            <b>No. of Openings</b>
-            <b style={{ marginLeft: '178px', paddingRight: '10px' }}>:</b> {selectedJob.Openings}
-          </Typography>
-          <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
-            <b> Company</b>
-            <b style={{ marginLeft: '220px', paddingRight: '10px' }}>:</b> {selectedJob.Company}
-          </Typography>
-          <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
-            <b> Location</b>
-            <b style={{ marginLeft: '225px', paddingRight: '10px' }}>:</b> {selectedJob.Location}
-          </Typography>
-          <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
-            <b> Worktype</b>
-            <b style={{ marginLeft: '221px', paddingRight: '10px' }}>:</b> {selectedJob.Worktype}
-          </Typography>
-          <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
-            <b> Qualification</b>
-            <b style={{ marginLeft: '200px', paddingRight: '10px' }}>:</b> {selectedJob.Education}
-          </Typography>
-          <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
-            <b> Year of Passing</b>
-            <b style={{ marginLeft: '180px', paddingRight: '10px' }}>:</b>{' '}
-            {!selectedJob.Year ? <span>Not Mentioned </span> : selectedJob.Year}
-          </Typography>
-          <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
-            <b> Experience</b>
-            <b style={{ marginLeft: '211px', paddingRight: '10px' }}>:</b> {selectedJob.ExperienceFrom} to{' '}
-            {selectedJob.ExperienceTo} Years
-          </Typography>
-          <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
-            <b> Description</b>
-            <b style={{ marginLeft: '210px', paddingRight: '10px' }}>:</b>
-            {selectedJob.Description}
-          </Typography>
-          <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
-            <b> ApplicationLink</b>
-            <b style={{ marginLeft: '183px', paddingRight: '10px' }}>:</b> {selectedJob.ApplicationLink}
-          </Typography>
-          <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
-            <b> Last Date to Apply</b>
-            <b style={{ marginLeft: '168px', paddingRight: '10px' }}>:</b> {selectedJob.Deadline}
-          </Typography>
-          <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
-            <b> Application Count</b>
-            <b style={{ marginLeft: '170px', paddingRight: '10px' }}>:</b> {selectedJob.ApplicationCount}
-          </Typography>
-          <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
-            <b> Selected</b>
-            <b style={{ marginLeft: '228px', paddingRight: '10px' }}>:</b> {selectedJob.SelectedCount}
-          </Typography>
-          <Typography sx={{ lineHeight: '4' }} variant="p" component="p">
-            <b> Remaining</b>
-            <b style={{ marginLeft: '215px', paddingRight: '10px' }}>:</b> {selectedJob.RemainingCount}
-          </Typography>
-        </Box>
-      </DialogContent>
-    </Box>
-  </>
-)}
-</Dialog>
-)
+  return (
+    <Dialog open={open} onClose={handleClose} maxWidth='md' fullWidth>
+      {selectedJob && (
+        <>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              background: '#2196f3',
+              marginBottom: '1px'
+            }}
+          >
+            <DialogTitle variant='h2' align='center'>
+              Candidate Application View
+            </DialogTitle>
+          </Box>
+          <Box sx={{ backgroundColor: '#f5f5f5' }}>
+            <DialogContent>
+              <Box>
+                <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <b> Job Role</b>
+                  <b style={{ marginLeft: '223px', paddingRight: '12px' }}>:</b>
+                  {selectedJob.position}
+                </Typography>
+                <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <b>Name</b>
+                  <b style={{ marginLeft: '242px', paddingRight: '10px' }}>:</b> {selectedJob.name}
+                </Typography>
+                <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <b> Email</b>
+                  <b style={{ marginLeft: '244px', paddingRight: '10px' }}>:</b> {selectedJob.email}
+                </Typography>
+                <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <b> Phone</b>
+                  <b style={{ marginLeft: '240px', paddingRight: '10px' }}>:</b> {selectedJob.phone}
+                </Typography>
+                <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <b> Qualification</b>
+                  <b style={{ marginLeft: '202px', paddingRight: '10px' }}>:</b> {selectedJob.qualification}
+                </Typography>
+                <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <b> Experience</b>
+                  <b style={{ marginLeft: '212px', paddingRight: '10px' }}>:</b> {selectedJob.experience} years
+                </Typography>
+                <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <b> Skills</b>
+                  <b style={{ marginLeft: '247px', paddingRight: '10px' }}>:</b> {selectedJob.skills}
+                </Typography>
+                <Typography sx={{ lineHeight: '4' }} variant='p' component='p'>
+                  <b> Applied Date</b>
+                  <b style={{ marginLeft: '200px', paddingRight: '12px' }}>:</b>
+                  {formatDate(selectedJob.createdAt)}
+                </Typography>
+              </Box>
+            </DialogContent>
+          </Box>
+        </>
+      )}
+    </Dialog>
+  )
 }
-export default ApplicationView;
+export default ApplicationView
