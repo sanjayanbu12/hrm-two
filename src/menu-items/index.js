d15d7fbf1d1e4fbe303bc594b2a2fd92c04e63c1
AjayB101 <ajayjadec@gmail.com>
2023-08-11 14:52:34 +0530

    role based auth

--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -4,7 +4,6 @@ import pages from './pages';
 // import other from './other';
 
 // ==============================|| MENU ITEMS ||============================== //
-
 const menuItems = {
   items: [dashboard, pages]
 };
