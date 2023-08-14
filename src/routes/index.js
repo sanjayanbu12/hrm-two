8b42ddfc311434e786507d57947ed26a16c6befa
AjayB101 <ajayjadec@gmail.com>
2023-08-14 14:26:33 +0530

    protected router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,33 @@
-import { useNavigate, useRoutes } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useLocation, useNavigate, useRoutes } from 'react-router-dom';
+import { useSelector,useDispatch } from 'react-redux';
 // routes
 import MainRoutes from './MainRoutes';
 import UserRoutes from './UserRoutes'
 import AuthenticationRoutes from './AuthenticationRoutes';
 import { useEffect } from 'react';
+import { LOGGED_OUT, USER_OR_NOT } from 'store/actions';
 
 // ==============================|| ROUTING RENDER ||============================== //
 function ThemeRoutes() {
   const isLoggedIn = useSelector(state => state.customization.isLoggedIn);
+  console.log(`isLoggedIn ${isLoggedIn}`)
   const isAdmin = useSelector(state=>state.customization.isAuthAdmin)
   const navigate = useNavigate()
+  const location = useLocation()
+  const dispatch = useDispatch()
+  const loginRoute=location.pathname.split('/')[3]
+  const protectRoute = () =>{
+   if(loginRoute==='login3'){
+    dispatch({type:LOGGED_OUT})
+    dispatch({type:USER_OR_NOT})
+   }
+  }
   useEffect(() => {
     { !isLoggedIn && navigate('/pages/login/login3') }
   }, [isLoggedIn])
+  useEffect(()=>{
+    protectRoute()
+  },[loginRoute])
   let route = useRoutes(isLoggedIn && isAdmin ? [AuthenticationRoutes, MainRoutes] : [AuthenticationRoutes,UserRoutes])
   return route
 }
