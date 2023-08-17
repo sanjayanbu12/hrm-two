a5f43e21bd8a75160869c02ba8c0e4d11bd2484b
sanjayanbu12 <134280780+sanjayanbu12@users.noreply.github.com>
2023-08-17 14:37:52 +0530

    Colors 001 (#40)
    * employee
    
    * employee
    
    * employeeform
    
    * details

--- src/views/addemployeetable/Viewdetails.js
+++ src/views/addemployeetable/Viewdetails.js
@@ -63,7 +63,7 @@ const Viewdetails = () => {
 
   return (
     <div>
-      <MainCard title="Employee Details">
+      <MainCard title="Employee Information">
         <Button
           onClick={() => Edit(employeedetails._id)}
           sx={{
@@ -79,141 +79,94 @@ const Viewdetails = () => {
           Edit
         </Button>
 
-        <MainCard title="Personal Details">
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-            <div>
-              <p>
-                <b>Employee ID</b>
-              </p>
-              <p>{employeedetails.employeeid}</p>
-            </div>
-            <div>
-              <p>
-                <b>Title</b>
-              </p>
-              <p>{employeedetails.title}</p>
-            </div>
-            <div>
-              <p>
-                <b>First Name</b>
-              </p>
-              <p>{employeedetails.name}</p>
-            </div>
-            <div>
-              <p>
-                <b>Last Name</b>
-              </p>
-              <p>{employeedetails.lastname}</p>
-            </div>
-            <div>
-              <p>
-                <b>Gender</b>
-              </p>
-              <p>{employeedetails.gender}</p>
-            </div>
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))', gridGap: '15px',marginLeft:'80px' }}>
+          <div >
+            <p><b>Employee ID</b></p>
+            <p>{employeedetails.employeeid}</p>
           </div>
-          <div style={{ display: 'flex', flexDirection: 'row', gap: '200px' }}>
-            <div>
-              <p>
-                <b>Mobile</b>
-              </p>
-              <p>{employeedetails.mob}</p>
-            </div>
-            <div>
-              <p>
-                <b>Email</b>
-              </p>
-              <p>{employeedetails.email}</p>
-            </div>
+          <div>
+            <p><b>Title</b></p>
+            <p>{employeedetails.title}</p>
           </div>
-        </MainCard>
-
-        <MainCard title="Personal Information">
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-            <div>
-              <p>
-                <b>Date of Birth</b>
-              </p>
-              <p>{formatteddob}</p>
-            </div>
-            <div>
-              <p>
-                <b>Blood Group</b>
-              </p>
-              <p>{employeedetails.bloodgroup}</p>
-            </div>
-            <div>
-              <p>
-                <b>Father`s Name</b>
-              </p>
-              <p>{employeedetails.fathername}</p>
-            </div>
+          <div>
+            <p><b>First Name</b></p>
+            <p>{employeedetails.name}</p>
           </div>
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-            <div>
-              <p>
-                <b>Nationality</b>
-              </p>
-              <p>{employeedetails.nationality}</p>
-            </div>
-
-            <div>
-              <p>
-                <b>Place of Birth</b>
-              </p>
-              <p>{employeedetails.placeofbirth}</p>
-            </div>
-            <div>
-              <p>
-                <b>Religion</b>
-              </p>
-              <p>{employeedetails.religion}</p>
-            </div>
-            <div>
-              <p>
-                <b>Personal Email</b>
-              </p>
-              <p>{employeedetails.peremail}</p>
-            </div>
+          <div>
+            <p><b>Last Name</b></p>
+            <p>{employeedetails.lastname}</p>
           </div>
-        </MainCard>
-        <MainCard title="Joining Details">
-          <div style={{ display: 'flex', flexDirection: 'row', gap: '250px' }}>
-            <div>
-              <p>
-                <b>Joined Date</b>
-              </p>
-              <p>{formattedjoin}</p>
-            </div>
-            <div>
-              <p>
-                <b>Status</b>
-              </p>
-              <p>{employeedetails.status}</p>
-            </div>
+          <div>
+            <p><b>Gender</b></p>
+            <p>{employeedetails.gender}</p>
           </div>
-        </MainCard>
-
-        <MainCard title="Personal Information">
-          <p>
-            <b>Department:</b> {employeedetails.dept}
-          </p>
-          <p>
-            <b>Permanent Address:</b> {employeedetails.peraddress}
-          </p>
-          <p>
-            <b>Joining Date:</b> {formattedjoin}
-          </p>
-          <p>
-            <b>Work Type:</b> {employeedetails.type}
-          </p>
-        </MainCard>
+          <div>
+            <p><b>Mobile</b></p>
+            <p>{employeedetails.mob}</p>
+          </div>
+          <div>
+            <p><b>Alternate Mobile</b></p>
+            <p>{employeedetails.altmob}</p>
+          </div>
+          <div>
+            <p><b>Email</b></p>
+            <p>{employeedetails.email}</p>
+          </div>
+          <div>
+            <p><b>Date of Birth</b></p>
+            <p>{formatteddob}</p>
+          </div>
+          <div>
+            <p><b>Blood Group</b></p>
+            <p>{employeedetails.bloodgroup}</p>
+          </div>
+          <div>
+            <p><b>Father`s Name</b></p>
+            <p>{employeedetails.fathername}</p>
+          </div>
+          <div>
+            <p><b>Nationality</b></p>
+            <p>{employeedetails.nationality}</p>
+          </div>
+          <div>
+            <p><b>Religion</b></p>
+            <p>{employeedetails.religion}</p>
+          </div>
+          <div>
+            <p><b>Reporting To</b></p>
+            <p>{employeedetails.report}</p>
+          </div>
+          <div>
+            <p><b>Joined Date</b></p>
+            <p>{formattedjoin}</p>
+          </div>
+          <div>
+            <p><b>Designation</b></p>
+            <p>{employeedetails.desi}</p>
+          </div>
+          <div>
+            <p><b>Department</b></p>
+            <p>{employeedetails.dept}</p>
+          </div>
+          <div>
+            <p><b>Work Type</b></p>
+            <p>{employeedetails.type}</p>
+          </div>
+          <div>
+            <p><b>Permanent Address</b></p>
+            <p>{employeedetails.peraddress}</p>
+          </div>
+          <div>
+            <p><b>Temporary Address</b></p>
+            <p>{employeedetails.temaddress}</p>
+          </div>
+        </div>
 
         <div
           style={{
-            position: 'fixed',
-            bottom: 30,
-            right: 50,
+            position: 'relative',
+            top: 20,
+            right: 10,
             zIndex: 9999,
             display: 'flex',
             justifyContent: 'flex-end'
