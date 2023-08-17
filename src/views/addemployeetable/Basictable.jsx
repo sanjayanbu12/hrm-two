a5898d74c8b995e7a852b65281e244cab90a614c
AjayB101 <ajayjadec@gmail.com>
2023-08-17 12:04:33 +0530

    table

--- src/views/addemployeetable/Basictable.jsx
+++ src/views/addemployeetable/Basictable.jsx
@@ -1,17 +1,51 @@
 import MaterialTable from "material-table";
 import tableIcons from "./MaterialTableIcons";
-
-const data = [
-  { name: "Mohammad", surname: "Faisal", birthYear: 1995 },
-  { name: "Nayeem Raihan ", surname: "Shuvo", birthYear: 1994 },
-];
-
+import axios from 'axios'
+import { useState,useEffect } from "react";
 const columns = [
+  { title: "EmployeeId", field: "employeeid" },
   { title: "Name", field: "name" },
-  { title: "Surname", field: "surname" },
-  { title: "Birth Year", field: "birthYear", type: "numeric" },
+  { title: "Designation", field: "desi" },
+  { title: "Gender", field: "gender" },
+  { title: "Email", field: "email" },
+  { title: "Type", field: "type" },
 ];
 
 export const BasicTable = () => {
-  return <MaterialTable title="Basic Table" columns={columns} data={data} icons={tableIcons} />;
+  const [edata, setedata] = useState([]);
+  const fetchEmployees = async () => {
+    const res = await axios.get(`https://hrm-backend-square.onrender.com/api/allemployee`);
+    setedata(res.data.reverse());
+    console.log(edata[1])
+  };
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
+  return <MaterialTable 
+  title="Basic Table" 
+  columns={columns} 
+  data={edata} 
+  icons={tableIcons} 
+  actions={[
+    {
+      icon: 'save',
+      tooltip: 'Save User',
+      isFreeAction: true,
+      onClick: (event, rowData) => alert("You saved " + rowData.name)
+    },
+    rowData => ({
+      icon: 'delete',
+      tooltip: 'Delete User',
+      isFreeAction: true,
+      onClick: (event, rowData) => confirm("You want to delete " + rowData.name),
+      disabled: rowData.birthYear < 2000
+    })
+  ]}
+
+  options={{
+    actionsColumnIndex:   6,    
+      exportButton: true
+
+  }}
+  />;
 };
