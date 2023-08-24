c337c401ceaa7d54ca528cfaa6b4db17fc4910f7
AjayB101 <ajayjadec@gmail.com>
2023-08-24 17:20:37 +0530

    org dta

--- src/views/recruitApproval/OrgTree.js
+++ src/views/recruitApproval/OrgTree.js
@@ -8,28 +8,60 @@ import { MapInteractionCSS } from 'react-map-interaction';
 import { useNavigate } from 'react-router';
 import CircularProgress from '@mui/material/CircularProgress';
 import Container from '@mui/material/Container';
-import { Button, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import Avatar from '@mui/material/Avatar';
 import { deepOrange } from '@mui/material/colors';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 const OrgTree = () => {
   const [loader, setLoaderStatus] = useState(true);
   const [orgMems, setorgMems] = useState([]);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [edata, setedata] = useState([]);
   const navigate = useNavigate();
   useEffect(() => {
     fetchOrgData();
   }, []);
+  useEffect(() => {
+    fetchEmployeesData();
+  }, []);
+
   const fetchOrgData = async () => {
     try {
       const response = await axios.get('https://hrm-backend-square.onrender.com/org/getorgs');
       setorgMems(response.data.orgData);
-      console.log(orgMems);
       setLoaderStatus(false);
     } catch (error) {
       console.log(error);
     }
+  };
+  const fetchEmployeesData = async () => {
+    try {
+      const response = await axios.get('https://hrm-backend-square.onrender.com/api/allemployee');
+      const employees = response.data;
+      setedata(employees);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+  const handleEmp = async (data) => {
+    const manData = {
+      managerName: {
+        name: data.name,
+        id: data._id
+      }
+    };
+    await axios.post('https://hrm-backend-square.onrender.com/org/createorg',manData);
+
   };
 
   return (
@@ -43,58 +75,63 @@ const OrgTree = () => {
             lineBorderRadius={'10px'}
             label={
               <div style={{ display: 'flex', justifyContent: 'center' }}>
-                {orgMems ? (
-                  
-                    <Card
-                      onClick={() => navigate('/managerapproval')}
-                      style={{
-                        width: '278px',
-                        height: '81px',
-                        backgroundColor: ' #EFE1FB',
-                        display:'flex',
-                        alignItems:'center',
-                        paddingLeft:'13px',
-                        paddingRight:'21px'
-                      }}
+                {orgMems.length > 1? (
+                  <Card
+                    onClick={() => navigate('/managerapproval')}
+                    style={{
+                      width: '278px',
+                      height: '81px',
+                      backgroundColor: ' #EFE1FB',
+                      display: 'flex',
+                      alignItems: 'center',
+                      paddingLeft: '13px',
+                      paddingRight: '21px'
+                    }}
+                  >
+                    <Container
+                      style={{ display: 'flex', justifyContent: 'space-between', width: '100%', alignItems: 'center' }}
+                      disableGutters={true}
                     >
-                      <Container style={{display:'flex',justifyContent:'space-between',width:'100%',alignItems:'center'}} disableGutters={true}>
-                        <div >
-                          <Avatar sx={{ bgcolor: deepOrange[500],color:'#fff',}}>
-                          H
-                          </Avatar>
-                          </div>
-                          <div  >
-                             <Typography variant='h3' fontSize={'18px'}>
-                               Ajay
-                             </Typography>
-                             <Typography variant='body2'>
-                               WebDeveloper
-                             </Typography>
-                          </div>
-                          <div>
-                            <IconButton>
-                              <ChevronRightIcon />
-                            </IconButton>
-                          </div>
-                      </Container>
-                    </Card>
-                  
+                      <div>
+                        <Avatar sx={{ bgcolor: deepOrange[500], color: '#fff' }}>{managerData.name}</Avatar>
+                      </div>
+                      <div>
+                        <Typography variant="h3" fontSize={'18px'}>
+                        {managerData.name}
+                        </Typography>
+                        <Typography variant="body2">WebDeveloper</Typography>
+                      </div>
+                      <div>
+                        <IconButton>
+                          <ChevronRightIcon />
+                        </IconButton>
+                      </div>
+                    </Container>
+                  </Card>
                 ) : (
                   <Container>
                     <Card
-                      onClick={() => navigate('/managerapproval')}
                       style={{
                         width: '278px',
                         height: '81px',
                         backgroundColor: ' #EFE1FB',
-                        display:'flex',
-                        alignItems:'center',
-                        justifyContent:'center'
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center'
                       }}
                     >
-                      <Button variant="text" sx={{color:'black',height:'73px',width:'21px',fontSize:'18px'}} startIcon={<AddIcon />}>
-                        Add
-                      </Button>
+                      <IconButton onClick={handleMenuOpen}>
+                        <AddIcon />
+                      </IconButton>
+                      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+                        {edata.map((data) => (
+                          <MenuItem onClick={() => handleEmp(data)} key={data._id}>
+                            {data.name}
+                          </MenuItem>
+                        ))}
+
+                        {/* Add more menu items as needed */}
+                      </Menu>
                     </Card>
                   </Container>
                 )}
@@ -103,79 +140,68 @@ const OrgTree = () => {
           >
             <TreeNode
               label={
-                <Card style={{
-                  width: '278px',
-                  height: '81px',
-                  backgroundColor: ' #E1EAFB',
-                  display:'flex',
-                  alignItems:'center',
-                  paddingLeft:'13px',
-                  paddingRight:'21px'
-                }} 
-                onClick={() => navigate('/hrapproval')}>
-                 <Container style={{display:'flex',justifyContent:'space-between',width:'100%',alignItems:'center'}} disableGutters={true}>
-                        <div >
-                          <Avatar sx={{ bgcolor: deepOrange[500],color:'#fff',}}>
-                          H
-                          </Avatar>
-                          </div>
-                          <div  >
-                             <Typography variant='h3' fontSize={'18px'}>
-                               Kannan
-                             </Typography>
-                             <Typography variant='body2'>
-                               Backend Dev 
-                             </Typography>
-                          </div>
-                          <div>
-                            <IconButton>
-                              <ChevronRightIcon />
-                            </IconButton>
-                          </div>
-                      </Container>
-                          </Card>
-              }
-            ></TreeNode>
-            <TreeNode
-              label={
-                <Card style={{
-                  width: '278px',
-                  height: '81px',
-                  backgroundColor: ' #E1EAFB',
-                  display:'flex',
-                  alignItems:'center',
-                  paddingLeft:'13px',
-                  paddingRight:'21px'
-                }} 
-                onClick={() => navigate('/hrapproval')}>
-                 <Container style={{display:'flex',justifyContent:'space-between',width:'100%',alignItems:'center'}} disableGutters={true}>
-                        <div >
-                          <Avatar sx={{ bgcolor: deepOrange[500],color:'#fff',}}>
-                          H
-                          </Avatar>
-                          </div>
-                          <div  >
-                             <Typography variant='h3' fontSize={'18px'}>
-                               Sanjay 
-                             </Typography>
-                             <Typography variant='body2'>
-                               Front End dev
-                             </Typography>
-                          </div>
-                          <div>
-                            <IconButton>
-                              <ChevronRightIcon />
-                            </IconButton>
-                          </div>
-                      </Container>
-          
+                <Card
+                  style={{
+                    width: '278px',
+                    height: '81px',
+                    backgroundColor: ' #E1EAFB',
+                    display: 'flex',
+                    alignItems: 'center',
+                    paddingLeft: '13px',
+                    paddingRight: '21px'
+                  }}
+                  onClick={() => navigate('/hrapproval')}
+                >
+                  <Container
+                    style={{ display: 'flex', justifyContent: 'space-between', width: '100%', alignItems: 'center' }}
+                    disableGutters={true}
+                  >
+                    <div>
+                      <Avatar sx={{ bgcolor: deepOrange[500], color: '#fff' }}>H</Avatar>
+                    </div>
+                    <div>
+                      <Typography variant="h3" fontSize={'18px'}>
+                        Kannan
+                      </Typography>
+                      <Typography variant="body2">Backend Dev</Typography>
+                    </div>
+                    <div>
+                      <IconButton>
+                        <ChevronRightIcon />
+                      </IconButton>
+                    </div>
+                  </Container>
                 </Card>
               }
             >
+              <TreeNode
+                label={
+                  <Card
+                    style={{
+                      height: '81px',
+                      backgroundColor: ' #E1EAFB',
+                      display: 'flex',
+                      alignItems: 'center',
+                      paddingLeft: '13px',
+                      paddingRight: '21px'
+                    }}
+                    onClick={() => navigate('/hrapproval')}
+                  >
+                    <Container
+                      style={{ display: 'flex', justifyContent: 'space-between', width: '100%', alignItems: 'center' }}
+                      disableGutters={true}
+                    >
+                      <div>
+                        <IconButton onClick={() => console.log('Add member clicked')}>
+                          <AddIcon />
+                        </IconButton>
+                      </div>
+                    </Container>
+                  </Card>
+                }
+              />
             </TreeNode>
-        
           </Tree>
-
         ) : (
           <CircularProgress sx={{ width: '100%', height: 'auto', position: 'absolute', top: '270px', left: '450px' }}></CircularProgress>
         )}
