8d845f9084698c7886b6e2432bf00fdf04e44e09
CyberHawk030 <cyber.hawk030@gmail.com>
2023-08-25 16:23:43 +0530

    skill

--- src/views/performance/HorizontalNonLinearStepper.jsx
+++ src/views/performance/HorizontalNonLinearStepper.jsx
@@ -1,33 +1,56 @@
-import React from 'react'
-// import Marker1 from '../../assets/images/icons/progress_1.svg';
-// import Marker2 from '../../assets/images/icons/progress_2.svg';
+import React from 'react';
+import Marker1 from '../../assets/images/icons/progress_1.svg';
+import Marker2 from '../../assets/images/icons/progress_2.svg';
+import { Typography,} from '@mui/material';
 
-const HorizontalNonLinearStepper = () => {
+
+
+const HorizontalNonLinearStepper = ({size,size1,size2, name}) => {
+  let mark2;
+  if(size2>=0){
+    mark2=size2-2.05;
+  }
+  else{
+    mark2=1000;
+  }
+  let mark1=size1-1.7;
+
+ size=size-1;
   return (
     <>
+    <div style={{  display: "flex",}}>
+      <div >
+        <Typography variant="h4"
+            body1="span"
+            sx={{
+              fontWeight: 500,
+              fontSize: 'small',
+              color: '#697586',
+              width: "40px",
+              marginTop: "2px",
+              whiteSpace: "nowrap"
+              
 
-    <div display="flex" height='65px'><svg width='100%' height='50px' display="flex">
+            }}>{name}</Typography>
+            </div>
+
+      <div  style={{ width:"99%", marginLeft:"140px", height: '20px'}}>
+        <svg width="100%" height="20px" style={{}}>
+          <g style={{}}>
+            <rect y="40%" fill="#eef2f6" width="100%" height="5"  />
+            <rect  y="40%"  fill="#3d5599" width = {`${size}%`} height="5"  />
+            <image height={"20px"} x={`${mark2}%`}  href={Marker2}  />
+            
+            <image y="15%" x={`${mark1}%`} href={Marker1}> </image>
+          </g>
+        </svg>
+      </div>
+
+      </div>
       
-    <g className='bars'>
 
-    <rect fill='#eef2f6' width='100%' height='25'></rect>;
-
-    <rect fill='#3d5599' width='30%' height='25'> </rect>;
-    <rect x="80%" fill='#000000' width='1%' height='25'> </rect>;
-    </g>
-
-
-    
-
-
-
-
-
-
-    </svg>
-    </div>
     </>
-  )
+  );
 }
 
-export default HorizontalNonLinearStepper
+export default HorizontalNonLinearStepper;
--- src/views/performance/SkillsetMatrix.jsx
+++ src/views/performance/SkillsetMatrix.jsx
@@ -1,26 +1,138 @@
-import React from 'react'
+import React from 'react';
 import MainCard from 'ui-component/cards/MainCard';
+import { List, ListItem, Typography } from '@mui/material';
+import Marker1 from '../../assets/images/icons/progress_1.svg';
+import Marker2 from '../../assets/images/icons/progress_2.svg';
 
 import HorizontalNonLinearStepper from './HorizontalNonLinearStepper';
 
 const SkillsetMatrix = () => {
   return (
     <>
-    <MainCard title="SkillSet Matrix" sx={{ height: '99%' }}>
-      
+      <MainCard title="SkillSet Matrix" sx={{ height: '99%' }}>
+        <div>
+          <List
+            sx={{
+              display: 'flex',
+              paddingRight: 0
+            }}
+          >
+            <ListItem>
+            <svg width="20px" height="20px">
+            <rect y="25%" fill="#3d5599" width="20px" height="10"  />
+            </svg>
+              <Typography
+                variant="h4"
+                body1="span"
+                sx={{
+                  fontWeight: 500,
+                  fontSize: 'small',
+                  color: '#697586',
+                  marginLeft: "15px"
 
+                }}
+              >
+                  - Current Skill Level
+              </Typography>
+              </ListItem>
+            <ListItem>
+              <img src={Marker1} alt="marker1" />
+              <Typography
+                variant="h4"
+                body1="span"
+                sx={{
+                  fontWeight: 500,
+                  fontSize: 'small',
+                  color: '#697586',
+                  marginLeft: "5px"
 
+                }}
+              >
+                  - Required Level of Skill
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <img src={Marker2} alt="marker2" />
+              <Typography
+                variant="h4"
+                body1="span"
+                sx={{
+                  fontWeight: 500,
+                  fontSize: 'small',
+                  color: '#697586',
+                  marginLeft: "5px"
+                }}
+              >
+                - Skill Goal
+              </Typography>
+            </ListItem>
+          </List>
+        </div>
+        <div>
+          <List
+            sx={{
+              display: 'flex',
+              paddingRight: 0,
+              marginLeft:"80px"
+            }}
+          >
+            <ListItem
+              sx={{
+                display: 'flex',
+                justifyContent: 'flex-end'
+              }}
+            >
+              Not Aware
+            </ListItem>
+            <ListItem
+              sx={{
+                display: 'flex',
+                justifyContent: 'flex-end'
+              }}
+            >
+              Awareness
+            </ListItem>
+            <ListItem
+              sx={{
+                display: 'flex',
+                justifyContent: 'center'
+              }}
+            >
+              Novice
+            </ListItem>
+            <ListItem
+              sx={{
+                display: 'flex',
+                justifyContent: 'flex-end'
+              }}
+            >
+              Competent
+            </ListItem>
+            <ListItem
+              sx={{
+                display: 'flex',
+                justifyContent: 'flex-end'
+              }}
+            >
+              Expert
+            </ListItem>
+          </List>
+        </div>
 
-
-      
-    <div><HorizontalNonLinearStepper /></div>
-    
+        <div>
+          <HorizontalNonLinearStepper size={50} size1={50} size2={72} name="Communication" />
+          <HorizontalNonLinearStepper size={27} size1={50} size2={50} name="Design Thinking" />
+          <HorizontalNonLinearStepper size={80} size1={60} size2={20} name="Problem Solving"/>
+          <HorizontalNonLinearStepper size={80} size1={60} size2={20} name="Remote Workspace" />
+          <HorizontalNonLinearStepper size={80} size1={60} size2={20} name="Administrative Work" />
+          <HorizontalNonLinearStepper size={80} size1={60} size2={20} name="Leadership" />
+          <HorizontalNonLinearStepper size={80} size1={60} size2={20} name="C" />
+          <HorizontalNonLinearStepper size={80} size1={60} size2={20} name="C++" />
+          <HorizontalNonLinearStepper size={80} size1={60} size2={20} name="Java" />
+          </div>
       </MainCard>
-
-
-
     </>
-  )
-}
+  );
+};
 
-export default SkillsetMatrix
+export default SkillsetMatrix;
