c8c8ac1d7f5c3f19bd03b74f84a5237228ea1251
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-09-01 10:12:31 +0530

    Update CourseOverview.jsx

--- src/views/learninganddevelopment/CourseOverview.jsx
+++ src/views/learninganddevelopment/CourseOverview.jsx
@@ -1,118 +1,107 @@
 import React, { useState, useEffect } from 'react';
-import { Dialog, DialogContent, DialogTitle, Paper, Grid, IconButton, Typography, Button, Card, CardContent, CardMedia } from '@mui/material';
-import MovieIcon from '@mui/icons-material/Movie';
-import CloseIcon from '@mui/icons-material/Close';
 import MainCard from 'ui-component/cards/MainCard';
-import axios from 'axios';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Paper, Dialog, DialogContent, Grid, IconButton } from '@mui/material';
+import MovieIcon from '@mui/icons-material/Movie';
 
-const MediaList = () => {
-  const [mediaList, setMediaList] = useState([]);
+const LearningModule = () => {
+  const [courses, setCourses] = useState([]);
   const [isVideoOpen, setIsVideoOpen] = useState(false);
-  const [currentVideo, setCurrentVideo] = useState('');
-  const [selectedMedia, setSelectedMedia] = useState(null);
+  const [currentCourse, setCurrentCourse] = useState(null);
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
 
-  const fetchMediaList = async () => {
-    try {
-      const response = await axios.get('http://localhost:3001/media/all');
-      const updatedMediaList = response.data.map((media) => ({
-        ...media,
-        videos: media.videos.map((video) => `http://localhost:3001${video}`)
-      }));
-      setMediaList(updatedMediaList);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchMediaList();
-  }, []);
-
-  const openVideoDialog = (videoUrl) => {
-    setCurrentVideo(videoUrl);
+  const openVideoDialog = (course) => {
     setIsVideoOpen(true);
+    setCurrentCourse(course);
+    setCurrentVideoIndex(0); // Reset the video index when opening a new video dialog
   };
 
   const closeVideoDialog = () => {
-    setCurrentVideo('');
     setIsVideoOpen(false);
+    setCurrentCourse(null);
+    setCurrentVideoIndex(0);
   };
 
-  const openMediaDialog = (media) => {
-    setSelectedMedia(media);
-  };
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/media/getAll');
+        const data = await response.json();
+        setCourses(data);
+      } catch (error) {
+        console.error('Error fetching courses:', error);
+      }
+    };
 
-  const closeMediaDialog = () => {
-    setSelectedMedia(null);
+    fetchCourses();
+  }, []);
+
+  const playVideo = (index) => {
+    setCurrentVideoIndex(index);
   };
 
   return (
-    <MainCard title="Course Upload Form">
-      {selectedMedia && (
-        <Dialog open={selectedMedia !== null} onClose={closeMediaDialog} fullWidth maxWidth="md">
-          <DialogTitle>
-            All Uploaded Videos
-            <IconButton aria-label="close" onClick={closeMediaDialog} sx={{ position: 'absolute', right: 8, top: 8 }}>
-              <CloseIcon />
-            </IconButton>
-          </DialogTitle>
-          <DialogContent style={{ maxHeight: 600, overflowY: 'auto' }}>
-            <Grid container spacing={0}>
-              {selectedMedia.videos.map((videoUrl, index) => (
-                <Grid item xs={12} key={index} style={{ marginBottom: '3px' }}>
-                  <MovieIcon onClick={() => openVideoDialog(videoUrl)} style={{ cursor: 'pointer', fontSize: 40 }} />
-                  <Typography variant="body2" sx={{ marginTop: 1 }}>
-                    Video {index + 1}
-                  </Typography>
-                </Grid>
-              ))}
-            </Grid>
-          </DialogContent>
-        </Dialog>
-      )}
-
-      <Dialog open={isVideoOpen} onClose={closeVideoDialog}>
-        <DialogContent>
-          <video controls autoPlay style={{ maxWidth: '100%' }}>
-            <track kind="captions" />
-            <source src={currentVideo} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </DialogContent>
-      </Dialog>
-
-      <Grid container spacing={3}>
-        {mediaList.map((media) => (
-          <Grid item xs={12} sm={4} md={4} key={media._id}>
-            <Paper elevation={2} sx={{ maxWidth: 300, borderRadius: '12px', height: 350 }}>
-              <Card>
-                <CardMedia
-                  sx={{ height: 110 }}
-                  image={media.thumbnailUrl}
-                  title={media.courseName}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {media.courseName}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {media.courseDescription}
-                  </Typography>
-                </CardContent>
-                <Button
-                  color="primary"
-                  onClick={() => openMediaDialog(media)}
-                  sx={{ display: 'block', margin: 'auto' }}
-                >
+    <MainCard title="Course Overview">
+      <Grid container spacing={2}>
+        {courses.map((course) => (
+          <Grid key={course._id} item xs={12} sm={6} md={4}>
+            <Paper
+              elevation={2}
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                maxWidth: 250,
+                borderRadius: '12px',
+              }}
+            >
+              <CardMedia sx={{ height: 90 }} image={course.image} title={course.courseName} />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {course.courseName}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {course.courseDescription}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button color="secondary" onClick={() => openVideoDialog(course)}>
                   View
                 </Button>
-              </Card>
+              </CardActions>
             </Paper>
           </Grid>
         ))}
+        <Dialog open={isVideoOpen} onClose={closeVideoDialog} maxWidth="lg">
+          <DialogContent>
+            {currentCourse && (
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  {currentCourse.videos.map((videoUrl, index) => (
+                    <IconButton key={index} onClick={() => playVideo(index)}>
+                      <MovieIcon />
+                    </IconButton>
+                  ))}
+                </Grid>
+                <Grid item xs={6}>
+                  {currentVideoIndex !== null && (
+                    <video controls width="100%" height="auto">
+                      <track kind="captions" srcLang="en" label="English captions" />
+                      <source src={currentCourse.videos[currentVideoIndex]} type="video/mp4" />
+                      Your browser does not support the video tag.
+                    </video>
+                  )}
+                </Grid>
+              </Grid>
+            )}
+          </DialogContent>
+        </Dialog>
       </Grid>
     </MainCard>
   );
 };
 
-export default MediaList;
+export default LearningModule;
