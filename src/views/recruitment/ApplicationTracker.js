7b677cac18338d485259fcde9d33a9c5e574e5fe
CyberHawk030 <cyber.hawk030@gmail.com>
2023-09-01 09:51:49 +0530

    Update ApplicationTracker.js

--- src/views/recruitment/ApplicationTracker.js
+++ src/views/recruitment/ApplicationTracker.js
@@ -10,12 +10,12 @@ import { saveAs } from 'file-saver';
 
 const columns = [
   { title: 'Name', field: 'Name', editable: false,Width:'24px'},
-  { title: 'Jobrole', field: 'Jobrole', editable: false },
+  { title: 'Jobrole', field: 'Jobrole', editable: false,width:'80px' },
   { title: 'Mobile No', field: 'MobileNo', sorting: false, editable: false },
   { title: 'Email', field: 'Email', sorting: false, editable: false },
   { title: 'Resume', field: 'Resume', sorting: false, editable: false },
-  { title: 'photo', field: 'Photo', sorting: false, editable: false },
-  { title: 'Applied Date', field: 'AppliedAt', type: 'date', sorting: false, editable: false },
+  { title: 'Photo', field: 'Photo', sorting: false, editable: false },
+  { title: 'Applied At', field: 'AppliedAt', type: 'date', sorting: false, editable: false},
   {
     title: 'Status',
     field: 'Status',
