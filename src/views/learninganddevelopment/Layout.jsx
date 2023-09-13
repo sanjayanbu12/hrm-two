bf1f5de90f4aaf0eded2683b6efd18aab0de2fcc
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-09-13 18:01:00 +0530

    Update Layout.jsx

--- src/views/learninganddevelopment/Layout.jsx
+++ src/views/learninganddevelopment/Layout.jsx
@@ -1,72 +1,44 @@
-import React from 'react'; 
+import React, { useState, useEffect } from 'react';
 import { PanelMenu } from 'primereact/panelmenu';
 import 'primereact/resources/primereact.min.css';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primeicons/primeicons.css';
 
-const Layout=()=> {
-    const items = [
-        {
-          label: 'Course name',
-          icon: 'pi pi-fw pi-server',
-          items: [
-            {
-              label: 'Module 1',
-              icon: 'pi pi-fw pi-folder-open',
-              items: [
-                {
-                  label: 'Video 1',
-                  icon: 'pi pi-fw pi-youtube'
-                },
-                {
-                  label: 'Video 2',
-                  icon: 'pi pi-fw pi-youtube'
-                },
-                {
-                  label: 'Video 3',
-                  icon: 'pi pi-fw pi-youtube'
-                }
-              ]
-            },
-            {
-              label: 'Module 2',
-              icon: 'pi pi-fw pi-folder-open',
-              items: [
-                {
-                  label: 'Video 4',
-                  icon: 'pi pi-fw pi-youtube'
-                },
-                {
-                  label: 'Video 5',
-                  icon: 'pi pi-fw pi-youtube'
-                }
-              ]
-            },
-            {
-              label: 'Module 3',
-              icon: 'pi pi-fw pi-folder-open',
-              items: [
-                {
-                  label: 'Video 1',
-                  icon: 'pi pi-fw pi-youtube'
-                },
-                {
-                  label: 'Video 2',
-                  icon: 'pi pi-fw pi-youtube'
-                },
-                {
-                  label: 'Video 3',
-                  icon: 'pi pi-fw pi-youtube'
-                }
-              ]
-            },
-          ]
-        }
-      ];
-    return (
-        <div className="card flex justify-content-center"style={{width:"100%"}} >
-            <PanelMenu model={items} className="w-full md:w-25rem"  />
-        </div>
-    )
-}
+const Layout = () => {
+  const [courseData, setCourseData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from MongoDB (replace with your actual API endpoint)
+    fetch('http://localhost:3001/videos/getall')
+      .then((response) => response.json())
+      .then((data) => {
+        setCourseData(data); // Assuming data structure matches the MongoDB response
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  // Function to generate the menu items based on fetched data
+  const generateMenuItems = () => {
+    return courseData.map((course) => {
+      return {
+        label: course.moduleName,
+        icon: 'pi pi-fw pi-folder-open',
+        items: course.videoUrls.map((videoUrl, index) => ({
+          label: `Video ${index + 1}`,
+          icon: 'pi pi-fw pi-youtube',
+          url: videoUrl,
+        })),
+      };
+    });
+  };
+
+  return (
+    <div className="card flex justify-content-center" style={{ width: '100%' }}>
+      <PanelMenu model={generateMenuItems()} className="w-full md:w-25rem" />
+    </div>
+  );
+};
+
 export default Layout;
