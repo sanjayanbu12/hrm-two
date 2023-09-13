a0a7e8444b047906b3f96b265f7c64ff9b89eecf
Prakash5072 <128066591+Prakash5072@users.noreply.github.com>
2023-09-13 12:03:55 +0530

    Prakash interview (#88)
    * Add Interview
    
    * .
    
    * .
    
    * Delete icons8-interview-64.png
    
    * update
    
    ---------
    
    Co-authored-by: CyberHawk030 <cyber.hawk030@gmail.com>
    Co-authored-by: Premnath M <134126704+Prem-ji@users.noreply.github.com>

--- src/views/recruitment/RecruitmentTable.js
+++ src/views/recruitment/RecruitmentTable.js
@@ -168,7 +168,7 @@ const navigate=useNavigate()
     <ThemeProvider theme={theme}>
       {Loader? (<div className='spinner' style={{position:'absolute',bottom:'40%',right:'45%'}} /> ):(
     <MaterialTable
-      title={<div style={{fontSize:'20px',marginTop:'10px',marginBottom:'10px'}}>Job Description Table</div>}
+      title={<div style={{fontSize:'20px',marginTop:'10px',marginBottom:'10px'}}>Position Details</div>}
       columns={columns}
       data={Adata}
       icons={tableIcons}
