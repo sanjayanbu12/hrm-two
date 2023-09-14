3aee6829333f37a169637b1fabf0ac9399fb3870
SanjayAnbu22 <134280780+sanjayanbu12@users.noreply.github.com>
2023-09-14 11:11:19 +0530

    table

--- src/views/addemployeetable/Basictable.jsx
+++ src/views/addemployeetable/Basictable.jsx
@@ -2,7 +2,7 @@ import MaterialTable from 'material-table';
 import tableIcons from './MaterialTableIcons';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router';
-import {  Box, Button} from '@mui/material';
+import { Box, Button } from '@mui/material';
 import TableViewIcon from '@mui/icons-material/TableView';
 import jsPDF from 'jspdf';
 
@@ -13,26 +13,42 @@ const columns = [
   { title: 'Designation', field: 'desi' },
   { title: 'Gender', field: 'gender' },
   { title: 'Email', field: 'email' },
-  { title: 'Type', field: 'type' },
+  { title: 'Type', field: 'type' }
 ];
-const csvColumns = ['EmployeeId', 'Name', 'Designation', 'Gender', 'Email', 'Type','AlternateMob','Reporting To','teamAddress','BloodGroup','Department','JoiningDate','LastName','MobileNumber','PermenentAddress'];
+const csvColumns = [
+  'EmployeeId',
+  'Name',
+  'Designation',
+  'Gender',
+  'Email',
+  'Type',
+  'AlternateMob',
+  'Reporting To',
+  'TempAddress',
+  'BloodGroup',
+  'Department',
+  'JoiningDate',
+  'LastName',
+  'MobileNumber',
+  'PermenentAddress'
+];
 export const BasicTable = () => {
   const [edata, setedata] = useState([]);
-const navigate=useNavigate()
+  const navigate = useNavigate();
   const fetchEmployeesData = async () => {
     try {
       const response = await axios.get('https://hrm-backend-square.onrender.com/api/allemployee');
       const employees = response.data.reverse(); // Reverse the data if needed
-      setedata(employees)
+      setedata(employees);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
-  const handleView = async(e,data) =>{
-    const id=data.map(x=>x._id)
-    console.log(id[0])
+  const handleView = async (e, data) => {
+    const id = data.map((x) => x._id);
+    console.log(id[0]);
     navigate(`/viewdetails/${id[0]}`);
-  }
+  };
   useEffect(() => {
     fetchEmployeesData();
   }, []);
@@ -52,7 +68,7 @@ const navigate=useNavigate()
       JoiningDate: item.join,
       LastName: item.lastname,
       MobileNumber: item.mob,
-      PermenentAddress: item.peraddress,
+      PermenentAddress: item.peraddress
     }));
     const csvHeaders = csvColumns.map((column) => column);
     const csvRows = [csvHeaders, ...csvData.map((item) => Object.values(item).map((value) => `"${value}"`))];
@@ -71,106 +87,126 @@ const navigate=useNavigate()
     const rows = data.map((item) => [
       item.employeeid,
       item.name,
-      item.desi,
-      item.gender,
+      item.lastname,
       item.email,
+      item.gender,
+      item.desi,
       item.type,
-      // item.cgpa,
-      // item.graduationYear,
-      // item.experience,
-      // item.sslc,
-      // item.hsc,
+      item.mob,
+      item.altmob,
+      item.report,
+      item.peraddress,
+      item.temaddress,
+      item.bloodgroup,
+      item.dept,
+      item.join
     ]);
-    const columnStyle={
-      0:{columnWidth:20},
-      1:{columnWidth:20},
-      2:{columnWidth:35},
-      3:{columnWidth:20},
-      4:{columnWidth:20},
-      5:{columnWidth:40},
-      // 6:{columnWidth:30},
-      // 7:{columnWidth:20},
-      // 8:{columnWidth:20},
-      // 9:{columnWidth:23},
-      // 10:{columnWidth:30},
-      // 11:{columnWidth:25},
-      // 12:{columnWidth:20},
-    }
-    const pdfHeaders = ['EmployeeId', 'Name', 'Designation ', 'Gender', 'Email', 'Type'];
+    const columnStyle = {
+      0: { columnWidth: 20 },
+      1: { columnWidth: 20 },
+      2: { columnWidth: 20 },
+      3: { columnWidth: 25 },
+      4: { columnWidth: 20 },
+      5: { columnWidth: 40 },
+      6: { columnWidth: 30 },
+      7: { columnWidth: 20 },
+      8: { columnWidth: 20 },
+      9: { columnWidth: 23 },
+      10: { columnWidth: 30 },
+      11: { columnWidth: 25 },
+      12: { columnWidth: 20 },
+      13: { columnWidth: 25 }
+    };
+    const pdfHeaders = [
+      'EmployeeId',
+      'Name',
+      'LastName',
+      'Email',
+      'Gender',
+      'Designation ',
+      'Type',
+      'Mobilenumber',
+      'AlternateMob',
+      'Reporting to',
+      'PermenentAddress',
+      'TempAddress',
+      'Bloodgroup',
+      'Department',
+      'Join'
+    ];
     pdf.autoTable({
       head: [pdfHeaders],
       body: rows,
       startY: 20,
-      columnStyle:columnStyle,
-      theme:'grid',
+      columnStyle: columnStyle,
+      theme: 'grid'
     });
 
     pdf.save('Employee_List.pdf');
   };
 
-
   return (
-   <>
-    <Box sx={{ flexGrow: 1, justifyContent: 'flex-end', display: 'flex' }}>
-    <Button
-      onClick={() => navigate(`/newemployee`)}
-      sx={{
-        background: 'rgba(33, 150, 243, 0.04)',
-        display: 'flex',
-        justifyContent: 'flex-end',
-        marginTop: '0px',
-        marginBottom: '0px'
-      }}
-    >
-      <TableViewIcon />
-      Form 
-    </Button>
-  </Box>
+    <>
+      <Box sx={{ flexGrow: 1, justifyContent: 'flex-end', display: 'flex' }}>
+        <Button
+          onClick={() => navigate(`/newemployee`)}
+          sx={{
+            background: 'rgba(33, 150, 243, 0.04)',
+            display: 'flex',
+            justifyContent: 'flex-end',
+            marginTop: '0px',
+            marginBottom: '0px'
+          }}
+        >
+          <TableViewIcon />
+          Form
+        </Button>
+      </Box>
 
-    <MaterialTable
-    raised={true}
-    title={<div style={{ fontWeight: 'bold' ,fontSize:'20px'}}>Employee Table</div>}
-      columns={columns}
-      data={edata}
-      icons={tableIcons}
-      actions={[
-        {
-          icon: tableIcons.Edit,
-          tooltip: 'View Details',
-          onClick: (event, rowData) => handleView(event,rowData)
-        },
-        
-        // {
-        //   icon: tableIcons.Clear,
-        //   tooltip: 'Edit',
-        //   onClick: (event, rowData) => alert(rowData.map(x=>x.name))
-        // },
-        // {
-        //   icon: tableIcons.Delete,
-        //   tooltip: 'Delete User',
-        //   onClick: (event, rowData) => confirm("You want to delete " + rowData.map(x=>x._id))
-        // }
-      ]}
-      style={{ boxShadow: '0px 2px 4px rgba(1, 1, 1, 1)' }}
-      options={{
-        actionsColumnIndex: 6,
-        exportButton: true,
-        exportCsv: exportCsv,
-        exportPdf: exportPdf,
-        grouping: true,
-        selection:true,
-        // rowStyle: {
-        //   backgroundColor: '#EEE',
-        // },
-        headerStyle: {
-          background: 'linear-gradient(180deg,#3a59af,#352786)',
-          color: '#fff', // Text color
-        },
-        headerCellStyle: {
-          color: 'white', // Text color for header cells
-        },
-      }}
-    />
+      <MaterialTable
+        raised={true}
+        title={<div style={{ fontWeight: 'bold', fontSize: '20px' }}>Employee Table</div>}
+        columns={columns}
+        data={edata}
+        icons={tableIcons}
+        actions={[
+          {
+            icon: tableIcons.Edit,
+            tooltip: 'View Details',
+            onClick: (event, rowData) => handleView(event, rowData)
+          }
+
+          // {
+          //   icon: tableIcons.Clear,
+          //   tooltip: 'Edit',
+          //   onClick: (event, rowData) => alert(rowData.map(x=>x.name))
+          // },
+          // {
+          //   icon: tableIcons.Delete,
+          //   tooltip: 'Delete User',
+          //   onClick: (event, rowData) => confirm("You want to delete " + rowData.map(x=>x._id))
+          // }
+        ]}
+        style={{ boxShadow: '0px 2px 4px rgba(1, 1, 1, 1)' }}
+        options={{
+          actionsColumnIndex: 6,
+          exportButton: true,
+          exportCsv: exportCsv,
+          exportPdf: exportPdf,
+          grouping: true,
+          selection: true,
+          // rowStyle: {
+          //   backgroundColor: '#EEE',
+          // },
+          headerStyle: {
+            background: 'linear-gradient(180deg,#3a59af,#352786)',
+            color: '#fff' // Text color
+          },
+          headerCellStyle: {
+            color: 'white' // Text color for header cells
+          }
+        }}
+      />
     </>
   );
 };
