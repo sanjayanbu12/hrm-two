9dafedd6707b671a5dfa9ea0a92fee0983262dba
CyberHawk030 <cyber.hawk030@gmail.com>
2023-09-14 09:48:08 +0530

    goals

--- src/views/performance/Goals/GoalTab.jsx
+++ src/views/performance/Goals/GoalTab.jsx
@@ -1,20 +1,45 @@
 import React from 'react';
 
-import { Paper, Stack } from '@mui/material';
-import GoalcardM from './GoalcardM';
+import { Paper,  Button, Grid,  } from '@mui/material';
 import Item from 'antd/es/list/Item';
+import Progressbar from './Progressbar';
+
 
 const GoalTab = () => {
   return (
-    <Paper style={{ padding: '20px', height:"430px", maxWidth:"100%", overflow:"auto"  }}>
-      <Stack display="flex" useFlexGap flexWrap="wrap" direction="row" spacing={{xs: 1, sm: 2}} sx={{listStyle:"none"}}>
-        <Item>
-          <GoalcardM />
-        </Item>
-        <Item>
-          <GoalcardM />
-        </Item>
-      </Stack>
+
+    <Paper style={{ padding: '20px', height:"fit-content", maxWidth:"100%" }}>
+
+      <div style={{display:"flex" ,justifyContent:'flex-end'}}><Button variant="contained" color="secondary" >
+            Set your Goal
+          </Button>
+          </div>
+          <Grid container spacing={1} sx={{listStyle:"none"}}>
+  <Grid item xs={6}>
+    <Item><Progressbar/></Item>
+  </Grid>
+  <Grid item xs={6}>
+    <Item><Progressbar/></Item>
+  </Grid>
+  <Grid item xs={6}>
+    <Item><Progressbar/></Item>
+  </Grid>
+  <Grid item xs={6}>
+    <Item><Progressbar/></Item>
+  </Grid>
+  <Grid item xs={6}>
+    <Item><Progressbar/></Item>
+  </Grid>
+  <Grid item xs={6}>
+    <Item><Progressbar/></Item>
+  </Grid>
+
+  </Grid>
+          
+
+            
+
+
     </Paper>
   );
 };
