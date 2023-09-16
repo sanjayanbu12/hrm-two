9b404dcdbbcc24a869d885ad34ac9ba42cddcd01
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-09-16 10:33:44 +0530

    route changed

--- src/views/coursemodule/CourseCatalog.jsx
+++ src/views/coursemodule/CourseCatalog.jsx
@@ -14,7 +14,7 @@ const CourseCatalog=()=> {
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/media/getAll')
+      .get('https://hrm-backend-square.onrender.com/media/getAll')
       .then((response) => {
         setCourseData(response.data);
         setLoading(false);
