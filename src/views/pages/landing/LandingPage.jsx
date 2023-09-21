e489f84eed1e2ca591e8b4ff24bec5500d6d54fc
SanjayAnbu22 <134280780+sanjayanbu12@users.noreply.github.com>
2023-09-21 15:34:44 +0530

    landing

--- src/views/pages/landing/LandingPage.jsx
+++ src/views/pages/landing/LandingPage.jsx
@@ -34,8 +34,8 @@ const LandingPage = () => {
               </div>
             </div>
             <div className="hero-figure anime-element">
-              <svg className="placeholder" width="528" height="396" viewBox="0 0 528 396">
-                <rect width="528" height="396" />
+              <svg className="placeholder" width="528" height="375" viewBox="0 0 528 396">
+                <rect width="528" height="375" />
               </svg>
               <div className="hero-figure-box hero-figure-box-01" data-rotation="45deg"></div>
               <div className="hero-figure-box hero-figure-box-02" data-rotation="-45deg"></div>
