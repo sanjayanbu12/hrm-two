97e3d64d875a816511668a478bdba78cf42b2c1a
SanjayAnbu22 <134280780+sanjayanbu12@users.noreply.github.com>
2023-09-25 12:47:40 +0530

    dashboard

--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -3,7 +3,7 @@ import { Grid } from '@mui/material';
 import 'react-toastify/dist/ReactToastify.css';
 import Upcomingevents from './Upcomingevents';
 import PopularCard from './PopularCard';
-import TotalGrowthBarChart from './TotalGrowthBarChart';
+import TotalOrderLineChartCard from './TotalOrderLineChartCard';
 import { gridSpacing } from 'store/constant';
 import Card from '@mui/material/Card';
 
@@ -12,28 +12,42 @@ const Dashboard = () => {
   useEffect(() => {
     setLoading(false);
   }, []);
-  
+
   return (
     <Card raised={true}>
-    <div style={{ overflow: 'hidden' }}>
+      <div style={{ overflow: 'hidden' }}>
+        <Grid item xs={12}>
+          <Grid container spacing={gridSpacing}>
+            <Grid item xs={12} md={3}>
+              <TotalOrderLineChartCard isLoading={isLoading} />
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <TotalOrderLineChartCard isLoading={isLoading} />
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <TotalOrderLineChartCard isLoading={isLoading} />
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <TotalOrderLineChartCard isLoading={isLoading} />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Grid container spacing={gridSpacing}>
+            <Grid item xs={12} md={8}>
+              <Upcomingevents isLoading={isLoading} />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <PopularCard isLoading={isLoading} />
+            </Grid>
+          </Grid>
+        </Grid>
 
         <Grid item xs={12} md={12}>
           <Upcomingevents isLoading={isLoading} />
-        </Grid>     
-      <Grid item xs={12}>
-        <Grid container spacing={gridSpacing}>
-          <Grid item xs={12} md={8}>
-            <TotalGrowthBarChart isLoading={isLoading} />
-          </Grid>
-           <Grid item xs={12} md={4}>
-            <PopularCard isLoading={isLoading} />
-          </Grid>         
         </Grid>
-      </Grid>
-      
-      <Grid container spacing={gridSpacing}>
-      </Grid>
-    </div>
+      </div>
     </Card>
   );
 };
