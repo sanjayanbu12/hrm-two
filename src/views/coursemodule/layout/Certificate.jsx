af96d9e478e61a4bf1f3595a8e9ee84b0ab51124
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-10-04 11:29:27 +0530

    main

--- src/views/coursemodule/layout/Certificate.jsx
+++ src/views/coursemodule/layout/Certificate.jsx
@@ -1,116 +1,104 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import logo from '../layout/SquareLogo.jpeg';
-import {StyledPaper,StyledBox} from './StyleCertificate';
+import { StyledPaper, StyledBox, FlexContainer,PaddedDiv,Coloredcontainer,Container} from './StyleCertificate';
 
 const Certificate = () => {
   return (
     <>
       <StyledPaper>
         <StyledBox>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Box sx={{ paddingLeft: '40px', paddingTop: '40px' }}>
+          <FlexContainer>
+            <PaddedDiv>
               <img style={{ display: 'flex' }} src={logo} alt="Square Logo" />
-            </Box>
-            
-            <Box sx={{ backgroundColor: '#F0DE36', height: '30px', width: '350px' }} />
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+            </PaddedDiv>
+            <h1 style={{ marginTop: '90px', display: 'block' }}>SNS Square Skill Training</h1>
+
+            <Coloredcontainer/>
+          </FlexContainer>
+          <Container>
             <Box
               sx={{
-              marginTop:'-170px',
+                marginTop: '-170px',
                 backgroundColor: '#F0DE36',
                 height: '85px',
                 width: '20px',
                 padding: '10px' // Add some padding to make the content visible
               }}
             />
-           
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          </Container>
+          <Container>
             <Box
               sx={{
-                marginTop:'-85px',
+                marginTop: '-85px',
                 backgroundColor: '#47B5FF',
                 height: '76px',
                 width: '20px',
                 padding: '10px' // Add some padding to make the content visible
               }}
             />
-           
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          </Container>
+          <Container>
             <Box
               sx={{
-                marginTop:'-10px',
+                marginTop: '-10px',
                 backgroundColor: '#FFB000',
                 height: '76px',
                 width: '20px',
                 padding: '10px' // Add some padding to make the content visible
               }}
             />
-           
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          </Container>
+          <Container>
             <Box
               sx={{
-               
                 backgroundColor: '#220160',
                 height: '76px',
                 width: '20px',
                 padding: '10px' // Add some padding to make the content visible
               }}
             />
-           
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          </Container>
+          <Container>
             <Box
               sx={{
-               
                 backgroundColor: '#cd0a0a',
                 height: '76px',
                 width: '20px',
                 padding: '10px' // Add some padding to make the content visible
               }}
             />
-           
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          </Container>
+          <Container>
             <Box
               sx={{
-               
                 backgroundColor: '#299a67',
                 height: '76px',
                 width: '20px',
                 padding: '10px' // Add some padding to make the content visible
               }}
             />
-           
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          </Container>
+          <Container>
             <Box
               sx={{
-               
                 backgroundColor: '#3458ad',
                 height: '76px',
                 width: '20px',
                 padding: '10px' // Add some padding to make the content visible
               }}
             />
-           
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          </Container>
+          <Container>
             <Box
               sx={{
-               
                 backgroundColor: '#c60c85',
                 height: '76px',
                 width: '20px',
                 padding: '10px'
               }}
             />
-           
-          </Box>
+          </Container>
         </StyledBox>
       </StyledPaper>
     </>
--- src/views/coursemodule/layout/StyleCertificate.js
+++ src/views/coursemodule/layout/StyleCertificate.js
@@ -1,5 +1,4 @@
-
-import { Box,Paper } from '@mui/material';
+import { Box, Paper } from '@mui/material';
 import { styled } from '@mui/system';
 
 export const StyledPaper = styled(Paper)(({ theme }) => ({
@@ -9,10 +8,29 @@ export const StyledPaper = styled(Paper)(({ theme }) => ({
 }));
 
 export const StyledBox = styled(Box)`
-&& {
-  height: 650px;
-  width: 100%;
-  background: white;
-}
+  && {
+    height: 650px;
+    width: 100%;
+    background: white;
+  }
 `;
- 
+export const FlexContainer = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const PaddedDiv = styled(Box)`
+  padding-left: 40px;
+  padding-top: 40px;
+`;
+export const Coloredcontainer = styled(Box)`
+  background-color: #f0de36;
+  height: 30px;
+  width: 350px;
+`;
+
+export const Container = styled(Box)`
+  display: flex;
+  justify-content: flex-end;
+`;
+
