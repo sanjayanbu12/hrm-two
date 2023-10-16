eff4ae7c6de7cf1a367893e0f4d2b5b8f656640b
AjayB101 <ajayjadec@gmail.com>
2023-10-16 12:48:12 +0530

    css work completed

--- src/views/leavemanagement/LeaveApproval.js
+++ src/views/leavemanagement/LeaveApproval.js
@@ -1,18 +1,58 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Divider } from 'primereact/divider';
-import { Card } from 'primereact/card';
+import { StyledContainer, StyledCard, parentStyle, StyledTypography } from './styled';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+import Grid from '@mui/material/Grid';
+import { Button } from 'primereact/button';
 const LeaveApproval = () => {
-  const userId=useSelector(state=>state.customization.userId)
-  console.log(userId)
+  const userId = useSelector((state) => state.customization.userId);
+  const [leaveData, setLeaveData] = useState([]);
+  console.log(userId);
+  const fetchLeave = async () => {
+    try {
+      const response = await axios.get('https://hrm-backend-square.onrender.com/api/leave');
+      setLeaveData(response.data.filter((leave) => leave.reportingto.status === false));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchLeave();
+  }, []);
   return (
     <div>
-      <Card title="Leave Request" >
-      <Divider />
-      <Card>
-        Hello
-      </Card>
-      </Card>
+      <StyledContainer title="Leave Request">
+        <Grid container spacing={2} rowSpacing={3} style={parentStyle}>
+          {leaveData.map((leave) => (
+            <Grid key={leave._id} xs={12} sm={6} md={6} lg={6}>
+              <StyledCard key={leave._id}>
+                <StyledTypography variant="h4">
+                  Name &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {leave.employeeName}
+                </StyledTypography>
+                <StyledTypography variant="h4">
+                  EmployeeId &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{leave.employeeId}
+                </StyledTypography>
+                <StyledTypography variant="h4">
+                  Type &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{leave.leaveType}
+                </StyledTypography>
+                <StyledTypography variant="h4">
+                  Days &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{leave.numberOfDays}
+                </StyledTypography>
+
+                <>
+                <Button text raised style={{marginRight:12}} icon="pi pi-check" rounded outlined aria-label="Filter" />
+                <Button text raised icon="pi pi-times" rounded outlined severity="danger" aria-label="Cancel" />
+                </>
+              </StyledCard>
+            </Grid>
+          ))}
+        </Grid>
+      </StyledContainer>
     </div>
   );
 };
---
+++ src/views/leavemanagement/styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import { Card } from 'primereact/card';
+import { Typography } from '@mui/material';
+export const StyledContainer=styled(Card)`
+display: flex;
+flex-wrap: wrap;
+width: 100%;
+`
+export const StyledCard=styled(Card)`
+background-color: antiquewhite;
+margin:10px 20px 10px 0;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items:center ;
+`
+export const StyledTypography=styled(Typography)`
+padding-bottom: 20px;
+`
+export const parentStyle={
+    margin:" 20px 20px 0 0",
+    padding: '10px'
+}
