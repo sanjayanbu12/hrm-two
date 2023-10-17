e2a51c0c1983bf8e26ac1741ee653f126400d877
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-10-17 16:55:52 +0530

    backend added

--- src/views/coursemodule/layout/Quiz.jsx
+++ src/views/coursemodule/layout/Quiz.jsx
@@ -1,96 +1,113 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { Button } from 'primereact/button';
 import { Toast } from 'primereact/toast';
 import { RadioButton } from 'primereact/radiobutton';
+import axios from 'axios';
+import { Skeleton } from 'primereact/skeleton';
 
-const Quiz=() =>{
-    const toast = useRef(null);
-    const [quizData, setQuizData] = useState([
-        {
-            question: 'What is the capital of France?',
-            options: ['Berlin', 'Madrid', 'Paris', 'Rome'],
-            correctAnswer: 'Paris',
-        },
-        {
-            question: 'Which planet is known as the Red Planet?',
-            options: ['Earth', 'Jupiter', 'Mars', 'Venus'],
-            correctAnswer: 'Mars',
-        },
-        
-    ]);
-    console.log(setQuizData)
+const Quiz = () => {
+  const toast = useRef(null);
 
-    const {
-        control,
-        handleSubmit,
-        reset,
-        formState: { errors },
-    } = useForm();
+  const [quizData, setQuizData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const show = (message) => {
-        toast.current.show({ severity: 'success', summary: 'Quiz Submitted', detail: message });
-    };
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors }
+  } = useForm();
 
-    const onSubmit = (data) => {
-        let score = 0;
+  const show = (message, score, totalQuestions) => {
+    let severity = 'success'; // Default to green color
 
-        // Calculate the user's score based on their answers
-        quizData.forEach((question, index) => {
-            if (data[`answer${index}`] === question.correctAnswer) {
-                score += 1;
-            }
-        });
+    if ((score / totalQuestions) * 100 < 60) {
+      severity = 'error'; // Change to red color for scores below 60%
+    }
 
-        show(`Your score: ${score}/${quizData.length}`);
-        reset();
-    };
+    toast.current.show({ severity, summary: 'Quiz Submitted', detail: message });
+  };
 
-    const getFormErrorMessage = (name) => {
-        return errors[name] ? <small className="p-error">{errors[name].message}</small> : <small className="p-error">&nbsp;</small>;
-    };
+  const onSubmit = (data) => {
+    let score = 0;
 
-    return (
-        < >
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex flex-column gap-2">
-                    <Toast ref={toast} />
+    // Calculate the user's score based on their answers
+    quizData.forEach((question, index) => {
+      if (data[`answer${index}`] === question.correctAnswer) {
+        score += 1;
+      }
+    });
 
-                    {quizData.map((question, index) => (
-                        <div key={index} className="flex flex-column gap-2">
-                            <div  style={{marginBottom:7}}>{question.question}</div>
-                            <Controller
-                                name={`answer${index}`}
-                                control={control}
-                                rules={{ required: 'Please select an answer.' }}
-                                render={({ field }) => (
-                                    <div className="flex">
-                                        {question.options.map((option, optionIndex) => (
-                                            <div  style={{marginTop:10}} key={optionIndex}>
-                                                <RadioButton
-                                               
-                                                    inputId={`q${index}o${optionIndex}`}
-                                                    {...field}
-                                                    value={option}
-                                                    checked={field.value === option}
-                                                />
-                                                <label htmlFor={`q${index}o${optionIndex}`} className="ml-1">
-                                                    {option}
-                                                </label>
-                                            </div>
-                                        ))}
-                                    </div>
-                                )}
-                            />
-                            {getFormErrorMessage(`answer${index}`)}
+    const totalQuestions = quizData.length;
+    show(`Your score: ${score}/${totalQuestions}`, score, totalQuestions);
+    reset();
+  };
+
+  const getFormErrorMessage = (name) => {
+    return errors[name] ? <small className="p-error">{errors[name].message}</small> : <small className="p-error">&nbsp;</small>;
+  };
+
+  useEffect(() => {
+    // Fetch quiz data from your backend API
+    axios
+      .get('http://localhost:3001/quiz/getall') // Replace with your actual API endpoint
+      .then((response) => {
+        setQuizData(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching quiz data:', error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-column gap-2">
+          <Toast ref={toast} />
+
+          {isLoading ? (
+            <div className="flex flex-wrap">
+              <div className="w-full md:w-6 p-3">
+                <Skeleton style={{ marginTop: 5 }} height="2rem" className="mb-2"></Skeleton>
+                <p></p>
+                <Skeleton height="4rem"></Skeleton>
+                <p></p>
+              </div>
+            </div>
+          ) : (
+            quizData.map((question, index) => (
+              <div key={index} className="flex flex-column gap-2">
+                <div style={{ marginBottom: 7 }}>{question.question}</div>
+                <Controller
+                  name={`answer${index}`}
+                  control={control}
+                  rules={{ required: 'Please select an answer.' }}
+                  render={({ field }) => (
+                    <div className="flex">
+                      {question.options.map((option, optionIndex) => (
+                        <div style={{ marginTop: 10 }} key={optionIndex}>
+                          <RadioButton inputId={`q${index}o${optionIndex}`} {...field} value={option} checked={field.value === option} />
+                          <label htmlFor={`q${index}o${optionIndex}`} className="ml-1">
+                            {option}
+                          </label>
                         </div>
-                    ))}
+                      ))}
+                    </div>
+                  )}
+                />
+                {getFormErrorMessage(`answer${index}`)}
+              </div>
+            ))
+          )}
 
-                    <Button label="Submit Quiz" type="submit" icon="pi pi-check" />
-                </div>
-            </form>
-        </>
-    );
-}
+          <Button label="Submit Quiz" type="submit" icon="pi pi-check" />
+        </div>
+      </form>
+    </>
+  );
+};
 
-export default Quiz
+export default Quiz;
--- src/views/coursemodule/layout/QuizForm.jsx
+++ src/views/coursemodule/layout/QuizForm.jsx
@@ -1,65 +1,114 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainCard from 'ui-component/cards/MainCard';
 import TextField from '@material-ui/core/TextField';
-import { Grid, Button } from '@mui/material';
+import { Grid, Button, IconButton, Box } from '@mui/material';
 import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@mui/icons-material/Delete';
+import axios from 'axios'; // Import Axios
 
 const useStyles = makeStyles((theme) => ({
   form: {
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   optionContainer: {
     display: 'flex',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
   },
   optionInput: {
-    marginRight: theme.spacing(2),
-    marginBottom: theme.spacing(2),
+    marginBottom: theme.spacing(2)
   },
   postButton: {
-    marginTop: theme.spacing(2),
-    animation: 'pulse 1s infinite',
+    marginTop: theme.spacing(2)
   },
-  '@keyframes pulse': {
-    '0%': {
-      transform: 'scale(1)',
-    },
-    '50%': {
-      transform: 'scale(1.1)',
-    },
-    '100%': {
-      transform: 'scale(1)',
-    },
-  },
+  deleteButton: {
+    color: 'red'
+  }
 }));
 
 const QuizForm = () => {
   const classes = useStyles();
+  const [options, setOptions] = useState(['','','','']);
+  const [question, setQuestion] = useState('');
+  const [correctAnswer, setCorrectAnswer] = useState('');
+
+  const addOption = () => {
+    const newOption = `Option ${options.length + 1}`;
+    setOptions([...options, newOption]);
+  };
+
+  const deleteOption = (index) => {
+    const updatedOptions = options.filter((_, i) => i !== index);
+    setOptions(updatedOptions);
+  };
+
+  const postQuiz = async () => {
+    try {
+      // Prepare the data to send to the backend
+      const data = {
+        question,
+        options,
+        correctAnswer
+      };
+
+      // Send a POST request to create a new quiz
+      const response = await axios.post('http://localhost:3001/quiz/create', data); // Replace with your actual API endpoint
+      console.log('Quiz created:', response.data);
+
+      // Reset the form
+      setQuestion('');
+      setOptions(['']);
+      setCorrectAnswer('');
+    } catch (error) {
+      console.error('Error creating quiz:', error);
+    }
+  };
 
   return (
-    <MainCard title="🧠 Quiz Form">
+    <MainCard title="Quiz Form">
       <form className={classes.form}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <TextField label="❓ Question" variant="outlined" fullWidth />
+            <TextField label="Question" variant="outlined" fullWidth value={question} onChange={(e) => setQuestion(e.target.value)} />
           </Grid>
-          <Grid item xs={12} className={classes.optionContainer}>
-            {[1, 2, 3, 4].map((optionNumber) => (
-              <TextField
-                key={`option${optionNumber}`}
-                label={`Option ${optionNumber}`}
-                variant="outlined"
-                className={classes.optionInput}
-              />
+          <Grid item xs={12}>
+            {options.map((option, index) => (
+              <Box key={`option${index}`} display="flex" alignItems="center">
+                <TextField
+                  label={`Option ${index + 1}`}
+                  variant="outlined"
+                  className={classes.optionInput}
+                  value={option}
+                  onChange={(e) => {
+                    const updatedOptions = [...options];
+                    updatedOptions[index] = e.target.value;
+                    setOptions(updatedOptions);
+                  }}
+                />
+                {index >= 2 && (
+                  <IconButton onClick={() => deleteOption(index)} className={classes.deleteButton}>
+                    <DeleteIcon />
+                  </IconButton>
+                )}
+              </Box>
             ))}
+            <Button variant="outlined" onClick={addOption}>
+              + Add Option
+            </Button>
           </Grid>
           <Grid item xs={12}>
-            <TextField label="✅ Correct Answer" variant="outlined" fullWidth />
+            <TextField
+              label="Correct Answer"
+              variant="outlined"
+              fullWidth
+              value={correctAnswer}
+              onChange={(e) => setCorrectAnswer(e.target.value)}
+            />
           </Grid>
           <Grid item xs={12}>
-            <Button type="submit" variant="contained" color="primary" className={classes.postButton}>
+            <Button type="button" variant="contained" color="primary" className={classes.postButton} onClick={postQuiz}>
               Post Quiz
             </Button>
           </Grid>
