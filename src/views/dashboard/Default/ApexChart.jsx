a258593ef42ca6800cb601af894f970cdcac6fb4
SanjayAnbu22 <134280780+sanjayanbu12@users.noreply.github.com>
2023-10-19 10:03:56 +0530

    loader

--- src/views/dashboard/Default/ApexChart.jsx
+++ src/views/dashboard/Default/ApexChart.jsx
@@ -1,28 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { Chart } from "react-google-charts";
-import {Card } from '@mui/material';
+import { Card, CircularProgress } from "@mui/material";
 
 export const options = {
   title: "",
   is3D: true,
 };
- 
 
 const Apexchart = () => {
   const [designationsData, setDesignationsData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://hrm-backend-square.onrender.com/api/allemployee")
       .then((response) => response.json())
       .then((data) => {
         const designations = data.map((employee) => employee.desi);
-        console.log("desi",designations)
         const designationCounts = countDesignations(designations);
         const chartData = convertToChartData(designationCounts);
         setDesignationsData(chartData);
+        setLoading(false); // Set loading to false when data is available
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
+        setLoading(false); // Set loading to false on error as well
       });
   }, []);
 
@@ -44,14 +45,20 @@ const Apexchart = () => {
 
   return (
     <Card elevation={2}>
-    <Chart
-      style={{marginLeft:'0px'}}
-      chartType="PieChart"
-      data={designationsData}
-      options={options}
-      width={"100%"}
-      height={"300px"}
-    />
+      {loading ? (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "300px" }}>
+          <CircularProgress />
+        </div>
+      ) : (
+        <Chart
+          style={{ marginLeft: "0px" }}
+          chartType="PieChart"
+          data={designationsData}
+          options={options}
+          width={"100%"}
+          height={"300px"}
+        />
+      )}
     </Card>
   );
 };
