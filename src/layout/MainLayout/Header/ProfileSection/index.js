1ff1b797f7f478cfd89a7fc3198a237f44fcd0f5
SanjayAnbu22 <134280780+sanjayanbu12@users.noreply.github.com>
2023-10-27 18:03:38 +0530

    css in landing page

--- src/layout/MainLayout/Header/ProfileSection/index.js
+++ src/layout/MainLayout/Header/ProfileSection/index.js
@@ -75,7 +75,7 @@ const ProfileSection = () => {
     } catch (error) {
       alert(error && error.message);
     }
-    // navigate('/pages/login/login3');
+    navigate('/pages/login/login3');
   };
   const now = new Date();
   const currentHour = now.getHours();
@@ -88,9 +88,7 @@ const ProfileSection = () => {
     greeting = 'Good Afternoon';
   } else if (currentHour >= 15 && currentHour < 19) {
     greeting = 'Good Evening';
-  } else {
-    greeting = 'Good Night';
-  }
+  } 
 
   const handleClose = (event) => {
     if (anchorRef.current && anchorRef.current.contains(event.target)) {
@@ -359,8 +357,8 @@ const ProfileSection = () => {
                           />
                         </ListItemButton> */}
                         <ListItemButton sx={{ borderRadius: `${customization.borderRadius}px` }} onClick={handleAccount}>
-                          <ListItemIcon color="green">
-                            <UserCircle size="1.3rem" strokeWidth={2} color={'black'} />
+                        <ListItemIcon>
+                            <IconLogout color="black" stroke={1.5} size="1.3rem" />
                           </ListItemIcon>
                           <ListItemText primary={<Typography variant="body2">Account Setting</Typography>} />
                         </ListItemButton>
