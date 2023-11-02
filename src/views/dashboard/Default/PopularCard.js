10303be6552c0cacb68b540ccbb8258a64eceb0f
AjayB101 <134268911+AjayB101@users.noreply.github.com>
2023-11-02 17:27:45 +0530

    Responsiveness (#129)
    * cotainer removed
    
    * Dash responsive
    
    * Update index.js

--- src/views/dashboard/Default/PopularCard.js
+++ src/views/dashboard/Default/PopularCard.js
@@ -15,21 +15,17 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
   {
-    imgPath:
-      'https://drive.google.com/uc?id=1vVlVqSIJIhNo7kDXkHHtpvPDBpytCoyJ',
+    imgPath: 'https://drive.google.com/uc?id=1vVlVqSIJIhNo7kDXkHHtpvPDBpytCoyJ'
   },
   {
-    imgPath:
-      'https://drive.google.com/uc?id=1WPRmDLjaCOQGiAB1lvLR6ark3oMh6am3',
+    imgPath: 'https://drive.google.com/uc?id=1WPRmDLjaCOQGiAB1lvLR6ark3oMh6am3'
   },
   {
-    imgPath:
-      'https://drive.google.com/uc?id=1aVl6bNacuONWrYEfh1iTHlct8L5-ONEg',
+    imgPath: 'https://drive.google.com/uc?id=1aVl6bNacuONWrYEfh1iTHlct8L5-ONEg'
   },
   {
-    imgPath:
-      'https://drive.google.com/uc?id=1nQnNJxPl33X3eAeBZ9ULL4DtXZw_cQTk',
-  },
+    imgPath: 'https://drive.google.com/uc?id=1nQnNJxPl33X3eAeBZ9ULL4DtXZw_cQTk'
+  }
 ];
 
 export default function PopularCard() {
@@ -50,78 +46,64 @@ export default function PopularCard() {
   };
   return (
     <>
-     <Card elevation={3}>
-    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
-    <Paper
-      square
-      elevation={0}
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        height: 0,
-        pl: 2,
-        bgcolor: '#ffff',
-      }}
-    >
-      <Typography>{images[activeStep].label}</Typography>
-    </Paper>
-    <AutoPlaySwipeableViews
-      axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-      index={activeStep}
-      onChangeIndex={handleStepChange}
-      enableMouseEvents
-    >
-      {images.map((step, index) => (
-        <div key={step.label}>
-          {Math.abs(activeStep - index) <= 2 ? (
-            <Box
-              component="img"
-              sx={{
-                height: 255,
-                display: 'block',
-                maxWidth: 400,
-                overflow: 'hidden',
-                width: '100%',
-              }}
-              src={step.imgPath}
-              alt={step.label}
-            />
-          ) : null}
-        </div>
-      ))}
-    </AutoPlaySwipeableViews>
-    <MobileStepper
-      steps={maxSteps}
-      position="static"
-      activeStep={activeStep}
-      nextButton={
-        <Button
-          size="small"
-          onClick={handleNext}
-          disabled={activeStep === maxSteps - 1}
-        >
-          Next
-          {theme.direction === 'rtl' ? (
-            <KeyboardArrowLeft />
-          ) : (
-            <KeyboardArrowRight />
-          )}
-        </Button>
-      }
-      backButton={
-        <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-          {theme.direction === 'rtl' ? (
-            <KeyboardArrowRight />
-          ) : (
-            <KeyboardArrowLeft />
-          )}
-          Back
-        </Button>
-      }
-    />
-  </Box>
-  </Card>
-
+      <Card elevation={3}>
+        <Box sx={{ flexGrow: 1 }}>
+          <Paper
+            square
+            elevation={0}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              height: 0,
+              pl: 2,
+              bgcolor: '#ffff'
+            }}
+          >
+            <Typography>{images[activeStep].label}</Typography>
+          </Paper>
+          <AutoPlaySwipeableViews
+            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+            index={activeStep}
+            onChangeIndex={handleStepChange}
+            enableMouseEvents
+          >
+            {images.map((step, index) => (
+              <div key={step.label}>
+                {Math.abs(activeStep - index) <= 2 ? (
+                  <Box
+                    component="img"
+                    sx={{
+                      height: 255,
+                      display: 'block',
+                      overflow: 'hidden',
+                      width: '100%'
+                    }}
+                    src={step.imgPath}
+                    alt={step.label}
+                  />
+                ) : null}
+              </div>
+            ))}
+          </AutoPlaySwipeableViews>
+          <MobileStepper
+            steps={maxSteps}
+            position="static"
+            activeStep={activeStep}
+            nextButton={
+              <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+                Next
+                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+              </Button>
+            }
+            backButton={
+              <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                Back
+              </Button>
+            }
+          />
+        </Box>
+      </Card>
     </>
   );
 }
--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -7,11 +7,11 @@ import Card1 from './Card1';
 import Card2 from './Card2';
 import Card3 from './Card3';
 import AnnualCard from './AnnualCard';
-import { gridSpacing } from 'store/constant';
+// import { gridSpacing } from 'store/constant';
 import Card from '@mui/material/Card';
 import Apexchart from './ApexChart';
 import TotalGrowthBarChart from './TotalGrowthBarChart';
-import Calendar from './Calendar'
+import Calendar from './Calendar';
 
 const Dashboard = () => {
   const [isLoading, setLoading] = useState(true);
@@ -20,60 +20,48 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <Card>
+    <Card sx={{p:2}} raised={true}>
       <div style={{ overflow: 'hidden' }}>
         <div style={{ fontWeight: 'bolder', fontSize: '25px', marginTop: '10px', marginLeft: '10px' }}>Welcome Admin!</div>
         <div style={{ fontWeight: 'bold', fontSize: '15px', marginLeft: '10px', marginBottom: '10px' }}>
           <p>Dashboard</p>
         </div>
-
-        <Grid item xs={12}>
-          <Grid container spacing={gridSpacing}>
-            <Grid item xs={12} md={3}>
-              <Card1 isLoading={isLoading} />
-            </Grid>
-            <Grid item xs={12} md={3}>
-              <Card2 isLoading={isLoading} />
-            </Grid>
-            <Grid item xs={12} md={3}>
-              <Card3 isLoading={isLoading} />
-            </Grid>
-            <Grid item xs={12} md={3}>
-              <AnnualCard isLoading={isLoading} />
-            </Grid>
-           
+        <Grid container rowSpacing={2} columnSpacing={2}>
+          <Grid item xs={12} md={6} sm={6} lg={3}>
+            <Card1 isLoading={isLoading} />
           </Grid>
-        </Grid>
-
-        <Grid item xs={12}>
-          <Grid container spacing={gridSpacing} style={{marginTop:'10px',marginBottom:'0px'}}>
-            <Grid item xs={12} md={8}>
-              <Apexchart isLoading={isLoading} />
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <PopularCard isLoading={isLoading} />
-            </Grid>
+          <Grid item xs={12} md={6} sm={6} lg={3}>
+            <Card2 isLoading={isLoading} />
           </Grid>
-        </Grid>
+          <Grid item xs={12} md={6} sm={6} lg={3}>
+            <Card3 isLoading={isLoading} />
+          </Grid>
+          <Grid item xs={12} md={6} sm={6} lg={3}>
+            <AnnualCard isLoading={isLoading} />
+          </Grid>
 
-        <Grid item xs={12}>
-          <Grid container spacing={gridSpacing} style={{marginTop:'0px'}}>
-          <Grid item xs={12} md={8}>
-          <TotalGrowthBarChart isLoading={isLoading} />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Calendar isLoading={isLoading} />
-        </Grid>
-        </Grid>
-        </Grid>
+          <Grid item xs={12} md={8} sm={12}>
+            <Apexchart isLoading={isLoading} />
+          </Grid>
+          <Grid item  xs={12} md={4} sm={6} lg={4}>
+            <PopularCard isLoading={isLoading} />
+          </Grid>
+          <Grid item  xs={12} md={5} sm={6} lg={5}>
+            <Calendar isLoading={isLoading} />
+          </Grid>
+          <Grid item xs={12} md={7} lg={7}>
+            <TotalGrowthBarChart isLoading={isLoading} />
+          </Grid>
+ 
 
-        {/* <Grid item xs={12} md={12}>
+          {/* <Grid item xs={12} md={12}>
           <Upcomingevents isLoading={isLoading} />
         </Grid> */}
 
-        {/* <Grid item xs={12} md={12}>
+          {/* <Grid item xs={12} md={12}>
           <Newevent isLoading={isLoading} />
         </Grid> */}
+        </Grid>
       </div>
     </Card>
   );
