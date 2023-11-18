e46e913ed7032ba5e8880765ddbce81a3feaa9f5
Surya Varadharajan <varadharajan025@gmail.com>
2023-11-18 16:57:45 +0530

    pushed

--- src/views/Precruitment/Finalstep.js
+++ src/views/Precruitment/Finalstep.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import InputAdornment from '@mui/material/InputAdornment';
 
 const Finalstep = () => {
   return (
     <>
-<Grid sx={{marginTop:'10px',display:"flex",justifyContent:'center',maxWidth:'600px'}} container spacing={4} >
-<Grid  item xs={10}>
-      <TextField sx={{width:'100%'}} label="Name"  />
-      </Grid>
-      <Grid item xs={10}>
-      <TextField sx={{width:'100%'}} label="Email" />
-      </Grid>
-      <Grid item xs={10}>
-      <TextField sx={{width:'100%'}} label="Product Description" />
-      </Grid>
+      <Grid sx={{ marginTop: '10px', display: "flex", justifyContent: 'center', maxWidth: '600px' }} container spacing={3} >
+        <Grid item xs={5}>
+          <TextField sx={{ width: '100%', ml: '10px' }} label="Vendor Name" />
+        </Grid>
+        <Grid item xs={7}>
+          <TextField sx={{ width: '100%'}} label="If Quote Available, Vendor Number"  InputProps={{
+            startAdornment: <InputAdornment position="start">+91 </InputAdornment>,
+          }} />
+        </Grid>
+        <Grid sx={{mt:'10px'}} item xs={11}>
+        <FormControl>
+      <FormLabel id="demo-row-radio-buttons-group-label">Whether Quote comparison done with other vendor?</FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+      >
+        <FormControlLabel value="Yes" control={<Radio  />} label="Yes" />
+        <FormControlLabel  value="No" control={<Radio  />} label="No" />
+       
+       
+      </RadioGroup>
+    </FormControl>
+        </Grid>
+          
 
-      <Grid item xs={10}>
-      <TextField sx={{width:'100%'}} label="Business justification"/>
+
       </Grid>
-     
-  
-    </Grid>
     </>
   )
 }
