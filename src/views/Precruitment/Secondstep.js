5d8d3035cda8c905e8729d8052ecfc9835b0fe6a
Surya Varadharajan <varadharajan025@gmail.com>
2023-11-23 18:15:44 +0530

    pushed

--- src/views/Precruitment/Secondstep.js
+++ src/views/Precruitment/Secondstep.js
@@ -12,62 +12,84 @@ import { green } from '@mui/material/colors';
 import InputAdornment from '@mui/material/InputAdornment';
 
 
-const Secondstep = () => {
-  return (
+const Secondstep = ({ setFormData, formData }) => {
+
+  const handleQuantityChange = (e) => {
+    setFormData({ ...formData, quantity: e.target.value });
+  };
+
+  const handleBudgetChange = (e) => {
+    setFormData({ ...formData, approximateBudget: e.target.value });
+  };
+
+  const handlePriorityChange = (e) => {
+    setFormData({ ...formData, priority: e.target.value });
+  };
+
+  const handleQuoteComparisonChange = (e) => {
+    setFormData({ ...formData, quoteComparison: e.target.value });
+  };
+ return (
     <>
-      <Grid sx={{ marginTop: '10px', display: "flex", justifyContent: 'center', maxWidth: '600px' }} container spacing={4}>
+      <Grid sx={{ marginTop: '10px', display: 'flex', justifyContent: 'center', maxWidth: '600px' }} container spacing={4}>
         <Grid item xs={10}>
-          <TextField sx={{ width: '100%' }} label="Quantity"   type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}/>
+          <TextField
+            sx={{ width: '100%' }}
+            label="Quantity"
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={handleQuantityChange}
+            value={formData.quantity || ''}
+          />
         </Grid>
         <Grid item xs={10}>
-        <TextField
-        sx={{ width: '100%' }}
-          label="Approximate Budget"
-          id="Approximate Budget"
-          InputProps={{
-            startAdornment: <InputAdornment position="start">₹</InputAdornment>,
-          }}
-        />
+          <TextField
+            sx={{ width: '100%' }}
+            label="Approximate Budget"
+            id="Approximate Budget"
+            InputProps={{
+              startAdornment: <InputAdornment position="start">₹</InputAdornment>,
+            }}
+            onChange={handleBudgetChange}
+            value={formData.approximateBudget || ''}
+          />
         </Grid>
         <Grid item xs={10}>
-        <FormControl>
-      <FormLabel id="demo-row-radio-buttons-group-label">Priority</FormLabel>
-      <RadioGroup
-        row
-        aria-labelledby="demo-row-radio-buttons-group-label"
-        name="row-radio-buttons-group"
-      >
-        <FormControlLabel value="High" control={<Radio sx={{
-          color: pink[800],
-          '&.Mui-checked': {
-            color: pink[600],
-          },
-        }} />} label="High" />
-        <FormControlLabel  value="Medium" control={<Radio sx={{
-          color: yellow[800],
-          '&.Mui-checked': {
-            color: yellow[600],
-          },
-        }} />} label="Medium" />
-        <FormControlLabel value="Low" control={<Radio sx={{
-          color: green[800],
-          '&.Mui-checked': {
-            color: green[600],
-          },
-        }} />} label="Low" />
-       
-      </RadioGroup>
-    </FormControl>
+          <FormControl>
+            <FormLabel id="demo-row-radio-buttons-group-label">Priority</FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+              onChange={handlePriorityChange}
+              value={formData.priority || ''}
+            >
+              <FormControlLabel value="High" control={<Radio sx={{ color: pink[800] }} />} label="High" />
+              <FormControlLabel value="Medium" control={<Radio sx={{ color: yellow[800] }} />} label="Medium" />
+              <FormControlLabel value="Low" control={<Radio sx={{ color: green[800] }} />} label="Low" />
+            </RadioGroup>
+          </FormControl>
         </Grid>
-        <Grid item xs={10}>
-          <TextField sx={{ width: '100%' }} label="Business justification" />
+        <Grid sx={{ mt: '10px' }} item xs={10}>
+          <FormControl>
+            <FormLabel id="demo-row-radio-buttons-group-label">Whether Quote comparison done with other vendor?</FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+              onChange={handleQuoteComparisonChange}
+              value={formData.quoteComparison || ''}
+            >
+              <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+              <FormControlLabel value="No" control={<Radio />} label="No" />
+            </RadioGroup>
+          </FormControl>
         </Grid>
       </Grid>
     </>
   );
-}
+};
 
 export default Secondstep;
