b8b006be1c5d79b2ab5b76a5738b3079281fd78e
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-11-24 16:04:02 +0530

    completed

--- src/views/coursemodule/layout/StepperQuiz.jsx
+++ src/views/coursemodule/layout/StepperQuiz.jsx
@@ -12,45 +12,67 @@ import { Toast } from 'primereact/toast';
 import { RadioButton } from 'primereact/radiobutton';
 import axios from 'axios';
 import { Skeleton } from 'primereact/skeleton';
-import { List, ListItem, ListItemText ,Divider} from '@mui/material';
+import { List, ListItem, ListItemText, Divider } from '@mui/material';
+import html2pdf from 'html2pdf.js';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
     padding: theme.spacing(3),
-    marginTop: theme.spacing(2),
+    marginTop: theme.spacing(2)
   },
   stepper: {
     background: 'transparent',
-    padding: theme.spacing(3, 0, 5),
+    padding: theme.spacing(3, 0, 5)
   },
   stepLabel: {
-    fontSize: '1.2rem',
+    fontSize: '1.2rem'
   },
   stepContent: {
-    marginTop: theme.spacing(2),
+    marginTop: theme.spacing(2)
   },
   buttonGroup: {
     display: 'flex',
     justifyContent: 'space-between',
-    marginTop: theme.spacing(3),
+    marginTop: theme.spacing(3)
   },
   iconButton: {
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.common.white,
     '&:hover': {
-      backgroundColor: theme.palette.primary.dark,
-    },
+      backgroundColor: theme.palette.primary.dark
+    }
   },
   icon: {
-    fontSize: 32,
+    fontSize: 32
+  }
+}));
+
+const useStyless = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(3),
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: theme.spacing(5),
+  },
+  title: {
+    textAlign: 'center',
+    marginBottom: theme.spacing(3),
+  },
+  content: {
+    textAlign: 'justify',
+    marginBottom: theme.spacing(3),
+  },
+  signature: {
+    textAlign: 'right',
   },
 }));
+
 
 const steps = [
   { label: 'Quiz', icon: <FeedIcon /> },
   { label: 'Quiz Score', icon: <ImportContactsIcon /> },
-  { label: 'Certificate', icon: <CardMembershipIcon /> },
+  { label: 'Certificate', icon: <CardMembershipIcon /> }
 ];
 
 const StepperQuiz = (courseid) => {
@@ -58,7 +80,7 @@ const StepperQuiz = (courseid) => {
   const [activeStep, setActiveStep] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(0);
-  const [quizData, setQuizData] = useState([]); // Add state for quizData
+  const [quizData, setQuizData] = useState([]);
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -96,16 +118,26 @@ const StepperQuiz = (courseid) => {
         <div className={classes.stepContent}>
           {activeStep === steps.length ? (
             <div>
-              <Typography variant="h5">All steps completed!</Typography>
-              <Button variant="contained" color="primary" onClick={handleReset}>
-                Restart
-              </Button>
+              <Typography variant="h5">Course completed!</Typography>
+            
             </div>
           ) : (
             <div>
-              {activeStep === 0 && <Quiz courseid={courseid} onNextStep={handleNext} onQuizSubmit={(answers, userScore, data) => { setUserAnswers(answers); setScore(userScore); setQuizData(data); }} />}
-              {activeStep === 1 && <Component2 quizData={quizData} userAnswers={userAnswers} score={score} />}
-              {activeStep === 2 && <Component3 />}
+              {activeStep === 0 && (
+                <Quiz
+                  courseid={courseid}
+                  onNextStep={handleNext}
+                  onQuizSubmit={(answers, userScore, data) => {
+                    setUserAnswers(answers);
+                    setScore(userScore);
+                    setQuizData(data);
+                  }}
+                />
+              )}
+              {activeStep === 1 && <QuizScore quizData={quizData} userAnswers={userAnswers} score={score} />}
+              {activeStep === 2 && (
+                <Certificate name="John Doe" course="React Advanced Course" date="November 24, 2023" signature="John Smith, Instructor" />
+              )}
             </div>
           )}
         </div>
@@ -116,13 +148,19 @@ const StepperQuiz = (courseid) => {
             </Button>
           </Grid>
           <Grid item>
-            <Grid item>
-              {activeStep !== 0 && (
-                <Button variant="contained" color="primary" onClick={handleNext} endIcon={<ArrowForwardIcon />}>
-                  {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                </Button>
-              )}
-            </Grid>
+            {activeStep !== 0 && (
+              <>
+                {score > quizData.length / 2 ? (
+                  <Button variant="contained" color="primary" onClick={handleNext} endIcon={<ArrowForwardIcon />}>
+                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                  </Button>
+                ) : (
+                  <Button variant="contained" color="primary" onClick={handleReset}>
+                    Retake Quiz
+                  </Button>
+                )}
+              </>
+            )}
           </Grid>
         </Grid>
       </Paper>
@@ -140,7 +178,7 @@ const Quiz = ({ courseid, onNextStep, onQuizSubmit }) => {
     control,
     handleSubmit,
     reset,
-    formState: { errors },
+    formState: { errors }
   } = useForm();
 
   const show = (message, score, totalQuestions) => {
@@ -243,13 +281,24 @@ const Quiz = ({ courseid, onNextStep, onQuizSubmit }) => {
   );
 };
 
-const Component2 = ({ quizData, userAnswers, score }) => {
+const QuizScore = ({ quizData, userAnswers, score }) => {
+  const totalQuestions = quizData.length;
+  const percentage = (score / totalQuestions) * 100;
+
   return (
     <>
       <Typography variant="h5" gutterBottom>
         Quiz Summary
       </Typography>
       <Typography variant="body1">Your Score: {score}</Typography>
+      <Typography variant="body1">Percentage: {percentage.toFixed(2)}%</Typography>
+
+      {percentage < 50 && (
+        <Typography variant="body1" style={{ color: 'red', marginTop: '10px' }}>
+          Your percentage is below 50%. Please go and retake the quiz.
+        </Typography>
+      )}
+
       <Divider style={{ margin: '10px 0' }} />
       <List>
         {quizData.map((question, index) => (
@@ -270,7 +319,7 @@ const Component2 = ({ quizData, userAnswers, score }) => {
                     component="div"
                     style={{
                       color: userAnswers[index] === question.correctAnswer ? 'green' : 'red',
-                      fontWeight: 'bold',
+                      fontWeight: 'bold'
                     }}
                   >
                     Your Answer: {userAnswers[index]}
@@ -287,8 +336,58 @@ const Component2 = ({ quizData, userAnswers, score }) => {
     </>
   );
 };
-const Component3 = () => {
-  return <Typography variant="body1">Share details about your work</Typography>;
+
+const Certificate = ({ name, course, date }) => {
+  const classes = useStyless();
+
+  const handleDownload = () => {
+    const element = document.getElementById('certificate');
+    if (element) {
+      const pdfOptions = {
+        margin: 10,
+        filename: 'certificate.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+      };
+
+      html2pdf().from(element).set(pdfOptions).save();
+    }
+  };
+
+  return (
+    <Paper id="certificate" className={classes.root} elevation={3}>
+      <Typography variant="h4" className={classes.title}>
+        Certificate of Completion
+      </Typography>
+      <Typography variant="h6" className={classes.content}>
+        This is to certify that
+      </Typography>
+      <Typography variant="h4" className={classes.content}>
+        {name}
+      </Typography>
+      <Typography variant="h6" className={classes.content}>
+        has successfully completed the course
+      </Typography>
+      <Typography variant="h5" className={classes.content}>
+        {course}
+      </Typography>
+      <Typography variant="h6" className={classes.content}>
+        on {date}.
+      </Typography>
+      <Grid container justifyContent="flex-end" className={classes.signature}>
+        {/* Add your digital signature image here */}
+        <img src="path/to/signature.png" alt="Signature" className={classes.signatureImage} />
+      </Grid>
+
+      <Grid container justifyContent="flex-end">
+        <Button variant="contained" color="primary" onClick={handleDownload}>
+          Download Certificate
+        </Button>
+      </Grid>
+    </Paper>
+  );
 };
+
 
 export default StepperQuiz;
--- src/views/coursemodule/layout/TabQuiz.jsx
+++ src/views/coursemodule/layout/TabQuiz.jsx
@@ -10,7 +10,7 @@ console.log(courseid,"This is tab quiz")
             <TabView>
                 <TabPanel header={<div style={{ marginLeft: 6 }}>Course Quiz</div>} leftIcon="pi pi-credit-card mr-2">
                    {/* <Quiz  courseid={courseid}/> */}
-                   <StepperQuiz courseid={courseid} />
+                   <StepperQuiz courseid={courseid}  />
                 </TabPanel>
                 <TabPanel header={<div style={{ marginLeft: 6 }}>Quiz Form</div>} leftIcon="pi pi-calculator mr-2">
                     <QuizForm courseid={courseid}/>
