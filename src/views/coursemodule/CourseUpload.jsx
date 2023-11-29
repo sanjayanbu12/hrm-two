4a32103a95eb50697c2addb4f7e98c84ea7e6162
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-11-29 16:08:00 +0530

    Update CourseUpload.jsx

--- src/views/coursemodule/CourseUpload.jsx
+++ src/views/coursemodule/CourseUpload.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
-import { TextField, Button,Grid, IconButton} from '@mui/material';
+import { TextField, Button, Grid, IconButton } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import MovieIcon from '@mui/icons-material/Movie';
 import ImageIcon from '@mui/icons-material/Image';
 import CancelIcon from '@mui/icons-material/Cancel';
-import { ToastContainer, toast } from 'react-toastify'; // Import react-toastify
-import 'react-toastify/dist/ReactToastify.css'; // Import react-toastify styles
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import MainCard from 'ui-component/cards/MainCard';
 import axios from 'axios';
 import { useDropzone } from 'react-dropzone';
 import Lottie from 'react-lottie';
-import upload from '../lottie/uploading.json'
-
+import upload from '../lottie/uploading.json';
 
 const uploading = {
   loop: true,
@@ -22,7 +21,6 @@ const uploading = {
   }
 };
 
-
 const CourseUpload = () => {
   const [courseName, setCourseName] = useState('');
   const [courseDescription, setCourseDescription] = useState('');
@@ -30,6 +28,7 @@ const CourseUpload = () => {
   const [videos, setVideos] = useState([]);
   const [selectedImages, setSelectedImages] = useState([]);
   const [selectedVideos, setSelectedVideos] = useState({});
+  const [durations, setDurations] = useState([]); // Added durations state
   const [errors, setErrors] = useState({});
   const [contentReady, setContentReady] = useState(true);
 
@@ -54,10 +53,41 @@ const CourseUpload = () => {
     setSelectedImages([selectedImage]);
   };
 
-  const handleVideoChange = (event) => {
+  const getVideoDuration = async (videoFile) => {
+    return new Promise((resolve, reject) => {
+      const video = document.createElement('video');
+      video.preload = 'metadata';
+
+      video.onloadedmetadata = () => {
+        resolve(video.duration);
+      };
+
+      video.onerror = (error) => {
+        reject(error);
+      };
+
+      video.src = URL.createObjectURL(videoFile);
+    });
+  };
+
+  const handleVideoChange = async (event) => {
     const selectedVideos = Array.from(event.target.files);
-    setVideos(selectedVideos);
-    setSelectedVideos(selectedVideos);
+    const videosWithDurations = [];
+
+    // Iterate through selected videos and get their durations
+    for (const video of selectedVideos) {
+      try {
+        const duration = await getVideoDuration(video);
+        videosWithDurations.push({ file: video, duration });
+      } catch (error) {
+        console.error('Error getting video duration:', error);
+        // Handle error if duration retrieval fails
+      }
+    }
+
+    setVideos(videosWithDurations);
+    setSelectedVideos(videosWithDurations);
+    setDurations(videosWithDurations.map((video) => video.duration)); // Update durations state
     setErrors((prevErrors) => ({ ...prevErrors, videos: '' }));
     event.target.value = '';
   };
@@ -67,6 +97,11 @@ const CourseUpload = () => {
     updatedVideos.splice(index, 1);
     setVideos(updatedVideos);
     setSelectedVideos(updatedVideos);
+    setDurations((prevDurations) => {
+      const updatedDurations = [...prevDurations];
+      updatedDurations.splice(index, 1);
+      return updatedDurations;
+    });
   };
 
   const handleRemoveImage = (index) => {
@@ -122,8 +157,11 @@ const CourseUpload = () => {
       formData.append('courseName', courseName);
       formData.append('courseDescription', courseDescription);
       formData.append('image', image);
+
+      // Append video files and their durations
       for (let i = 0; i < videos.length; i++) {
-        formData.append('videos', videos[i]);
+        formData.append('videos', videos[i].file);
+        formData.append('durations', durations[i]); // Add durations to FormData
       }
 
       setContentReady(false);
@@ -143,6 +181,7 @@ const CourseUpload = () => {
       setVideos([]);
       setSelectedImages([]);
       setSelectedVideos({});
+      setDurations([]); // Reset durations state
       setErrors({});
     } catch (error) {
       console.error(error);
@@ -159,108 +198,100 @@ const CourseUpload = () => {
 
   return (
     <MainCard title="Course Upload Form">
-      {/* <Paper elevation={3} style={{ padding: '16px' }}> */}
-        {!contentReady ? (
-      
-       <Lottie
-         options={uploading}
-         height={310} 
-         width="50%"
-       />
-    
-        ) : (
-          <form onSubmit={handleSubmit}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  label="Course Name"
-                  placeholder="Enter course name"
-                  variant="outlined"
-                  value={courseName}
-                  onChange={handleCourseNameChange}
-                  fullWidth
-                  error={!!errors.courseName}
-                  helperText={errors.courseName}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  label="Course Description"
-                  placeholder="Enter course description"
-                  variant="outlined"
-                  value={courseDescription}
-                  onChange={handleCourseDescriptionChange}
-                  fullWidth
-                  multiline
-                  rows={3}
-                  error={!!errors.courseDescription}
-                  helperText={errors.courseDescription}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <div {...getRootProps()} style={{ cursor: 'pointer' }}>
-                  <input {...getInputProps()} />
-                  <Button variant="outlined" color="primary" startIcon={<CloudUploadIcon />} style={{ minWidth: 195 }}>
-                    Upload Course Image
-                  </Button>
-                </div>
-                {selectedImages.length > 0 && (
-                  <div style={{ marginTop: '8px' }}>
-                    <h3>Selected Images:</h3>
-                    <ul>
-                      {selectedImages.map((img, index) => (
-                        <li key={index}>
-                          <ImageIcon style={{ verticalAlign: 'middle', marginRight: '4px' }} />
-                          {img.name}
-                          <IconButton
-                            color="secondary"
-                            onClick={() => handleRemoveImage(index)}
-                            style={{ verticalAlign: 'middle', marginLeft: '30px' }}
-                          >
-                            <CancelIcon />
-                          </IconButton>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                )}
-                {errors.image && <div style={{ color: 'red', marginTop: '8px' }}>{errors.image}</div>}
-              </Grid>
-              <Grid item xs={12}>
-                <label htmlFor="video-input">
-                  <input type="file" id="video-input" accept="video/*" multiple onChange={handleVideoChange} style={{ display: 'none' }} />
-                  <Button variant="outlined" color="primary" component="span" startIcon={<CloudUploadIcon />} style={{ minWidth: 195 }}>
-                    Upload Course Videos
-                  </Button>
-                </label>
-                {errors.videos && <div style={{ color: 'red', marginTop: '8px' }}>{errors.videos}</div>}
-                {selectedVideos.length > 0 && (
-                  <div style={{ marginTop: '8px' }}>
-                    <h3>Selected Videos:</h3>
-                    <ul>
-                      {selectedVideos.map((video, index) => (
-                        <li key={index}>
-                          <MovieIcon style={{ verticalAlign: 'middle', marginRight: '4px' }} />
-                          {video.name}
-                          <IconButton color="secondary" onClick={() => handleRemoveVideo(index)} style={{ verticalAlign: 'middle' }}>
-                            <CancelIcon />
-                          </IconButton>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                )}
-              </Grid>
-              <Grid item xs={12}>
-                <Button type="submit" variant="contained" color="primary">
-                  Create Media Course
-                </Button>
-              </Grid>
+      {!contentReady ? (
+        <Lottie options={uploading} height={310} width="50%" />
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                label="Course Name"
+                placeholder="Enter course name"
+                variant="outlined"
+                value={courseName}
+                onChange={handleCourseNameChange}
+                fullWidth
+                error={!!errors.courseName}
+                helperText={errors.courseName}
+              />
             </Grid>
-          </form>
-        )}
-        <ToastContainer />
-      {/* </Paper> */}
+            <Grid item xs={12}>
+              <TextField
+                label="Course Description"
+                placeholder="Enter course description"
+                variant="outlined"
+                value={courseDescription}
+                onChange={handleCourseDescriptionChange}
+                fullWidth
+                multiline
+                rows={3}
+                error={!!errors.courseDescription}
+                helperText={errors.courseDescription}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <div {...getRootProps()} style={{ cursor: 'pointer' }}>
+                <input {...getInputProps()} />
+                <Button variant="outlined" color="primary" startIcon={<CloudUploadIcon />} style={{ minWidth: 195 }}>
+                  Upload Course Image
+                </Button>
+              </div>
+              {selectedImages.length > 0 && (
+                <div style={{ marginTop: '8px' }}>
+                  <h3>Selected Images:</h3>
+                  <ul>
+                    {selectedImages.map((img, index) => (
+                      <li key={index}>
+                        <ImageIcon style={{ verticalAlign: 'middle', marginRight: '4px' }} />
+                        {img.name}
+                        <IconButton
+                          color="secondary"
+                          onClick={() => handleRemoveImage(index)}
+                          style={{ verticalAlign: 'middle', marginLeft: '30px' }}
+                        >
+                          <CancelIcon />
+                        </IconButton>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {errors.image && <div style={{ color: 'red', marginTop: '8px' }}>{errors.image}</div>}
+            </Grid>
+            <Grid item xs={12}>
+              <label htmlFor="video-input">
+                <input type="file" id="video-input" accept="video/*" multiple onChange={handleVideoChange} style={{ display: 'none' }} />
+                <Button variant="outlined" color="primary" component="span" startIcon={<CloudUploadIcon />} style={{ minWidth: 195 }}>
+                  Upload Course Videos
+                </Button>
+              </label>
+              {errors.videos && <div style={{ color: 'red', marginTop: '8px' }}>{errors.videos}</div>}
+              {selectedVideos.length > 0 && (
+                <div style={{ marginTop: '8px' }}>
+                  <h3>Selected Videos:</h3>
+                  <ul>
+                    {selectedVideos.map((video, index) => (
+                      <li key={index}>
+                        <MovieIcon style={{ verticalAlign: 'middle', marginRight: '4px' }} />
+                        {`${video.file.name} (${Math.round(video.duration)} seconds)`}
+                        <IconButton color="secondary" onClick={() => handleRemoveVideo(index)} style={{ verticalAlign: 'middle' }}>
+                          <CancelIcon />
+                        </IconButton>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </Grid>
+            <Grid item xs={12}>
+              <Button type="submit" variant="contained" color="primary">
+                Create Media Course
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      )}
+      <ToastContainer />
     </MainCard>
   );
 };
